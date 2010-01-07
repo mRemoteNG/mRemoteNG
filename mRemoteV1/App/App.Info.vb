c5cc8b452d0a6fd73676333394fab559f2ba6489
Riley McArdle <riley@glyff.net>
2010-01-07 16:41:01 -0600

    Changed website addresses.

--- mRemoteV1/App/App.Info.vb
+++ mRemoteV1/App/App.Info.vb
@@ -3,10 +3,10 @@ Imports System.Environment
 Namespace App
     Namespace Info
         Public Class General
-            Public Shared ReadOnly URLHome As String = "http://www.mRemote.org/"
-            Public Shared ReadOnly URLDonate As String = "https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=felix%2edeimel%40gmail%2ecom&item_name=mRemote%20Donations&buyer_credit_promo_code=&buyer_credit_product_category=&buyer_credit_shipping_method=&buyer_credit_user_address_change=&no_shipping=0&no_note=1&tax=0&currency_code=EUR&lc=US&bn=PP%2dDonationsBF&charset=UTF%2d8"
-            Public Shared ReadOnly URLBugs As String = "http://bugs.mremote.org/"
-            Public Shared ReadOnly URLAnnouncement As String = "http://update.mRemote.org/mRemote_Announcment.txt"
+            Public Shared ReadOnly URLHome As String = "http://www.mRemoteNG.org/"
+            Public Shared ReadOnly URLDonate As String = "http://donate.mRemoteNG.org/"
+            Public Shared ReadOnly URLBugs As String = "http://bugs.mRemoteNG.org/"
+            Public Shared ReadOnly URLAnnouncement As String = "http://update.mRemoteNG.org/announcement.txt"
             Public Shared ReadOnly LogFile As String = GetFolderPath(SpecialFolder.LocalApplicationData) & "\mRemoteNG.log"
             Public Shared ReadOnly HomePath As String = My.Application.Info.DirectoryPath
             Public Shared EncryptionKey As String = "mR3m"
@@ -25,11 +25,11 @@ Namespace App
         End Class
 
         Public Class Update
-            Public Shared ReadOnly URL As String = "http://update.mRemote.org/"
+            Public Shared ReadOnly URL As String = "http://update.mRemoteNG.org/"
 #If DEBUG Then
-            Public Shared ReadOnly File As String = "mRemote_Update_Debug.txt"
+            Public Shared ReadOnly File As String = "update-debug.txt"
 #Else
-            Public Shared ReadOnly File As String = "mRemote_Update.txt"
+            Public Shared ReadOnly File As String = "update.txt"
 #End If
         End Class
 
