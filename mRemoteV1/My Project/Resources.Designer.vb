0bed923a4d03bf005da17c6edbbc142ecee9199b
Riley McArdle <riley@mremoteng.org>
2011-06-05 12:06:07 -0500

    Added code to verify database version and upgrade database from 2.2 (mRemoteNG 1.66 or earlier) to 2.3 (mRemoteNG 1.67).

--- mRemoteV1/My Project/Resources.Designer.vb
+++ mRemoteV1/My Project/Resources.Designer.vb
@@ -2151,6 +2151,15 @@ Namespace My.Resources
         End Property
         
         '''<summary>
+        '''  Looks up a localized string similar to The database version {0} is not compatible with this version of {1}..
+        '''</summary>
+        Friend ReadOnly Property strErrorBadDatabaseVersion() As String
+            Get
+                Return ResourceManager.GetString("strErrorBadDatabaseVersion", resourceCulture)
+            End Get
+        End Property
+        
+        '''<summary>
         '''  Looks up a localized string similar to Error code {0}..
         '''</summary>
         Friend ReadOnly Property strErrorCode() As String
@@ -2160,11 +2169,29 @@ Namespace My.Resources
         End Property
         
         '''<summary>
+        '''  Looks up a localized string similar to The connection list could not be saved..
+        '''</summary>
+        Friend ReadOnly Property strErrorConnectionListSaveFailed() As String
+            Get
+                Return ResourceManager.GetString("strErrorConnectionListSaveFailed", resourceCulture)
+            End Get
+        End Property
+        
+        '''<summary>
         '''  Looks up a localized string similar to Errors.
         '''</summary>
         Friend ReadOnly Property strErrors() As String
             Get
                 Return ResourceManager.GetString("strErrors", resourceCulture)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized string similar to VerifyDatabaseVersion (Config.Connections.Save) failed. {0}.
+        '''</summary>
+        Friend ReadOnly Property strErrorVerifyDatabaseVersionFailed() As String
+            Get
+                Return ResourceManager.GetString("strErrorVerifyDatabaseVersionFailed", resourceCulture)
             End Get
         End Property
         
