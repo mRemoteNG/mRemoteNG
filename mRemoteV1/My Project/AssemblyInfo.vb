87b0f317d1617665cc408f6344203aa198ce3ae0
Riley McArdle <riley@mremoteng.org>
2011-07-29 02:12:11 -0500

    Updated AssemblyVersion to 1.69.

--- mRemoteV1/My Project/AssemblyInfo.vb
+++ mRemoteV1/My Project/AssemblyInfo.vb
@@ -31,4 +31,4 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.68.*")> 
+<Assembly: AssemblyVersion("1.69.*")> 
