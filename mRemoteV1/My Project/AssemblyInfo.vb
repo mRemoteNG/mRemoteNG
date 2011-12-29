eff877169d556f7cb488d0fc29d03b143b424ac7
rmcardle <riley@mremoteng.org>
2011-12-28 20:10:00 -0600

    Change version number to 1.70.

--- mRemoteV1/My Project/AssemblyInfo.vb
+++ mRemoteV1/My Project/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Resources
+Imports System.Resources
 
 Imports System.Reflection
 Imports System.Runtime.InteropServices
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.69.*")> 
+<Assembly: AssemblyVersion("1.70.*")>
 
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
