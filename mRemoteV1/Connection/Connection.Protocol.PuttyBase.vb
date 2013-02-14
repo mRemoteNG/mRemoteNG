6d15e3649e4982aece46af7cd6f1a79e484e7404
Riley McArdle <riley@glyff.net>
2013-02-14 14:12:48 -0600

    Fix issue MR-71 - Minimizing mRemoteNG causes temporary re-size of Putty sessions (windows)
    Fix issue MR-199 - when using screen inside putty, screen becomes dead when reduce mremoteNG
    Fix issue MR-284 - SSH: Text not showing properly

--- mRemoteV1/Connection/Connection.Protocol.PuttyBase.vb
+++ mRemoteV1/Connection/Connection.Protocol.PuttyBase.vb
@@ -191,6 +191,8 @@ Namespace Connection
 
             Public Overrides Sub Resize()
                 Try
+                    If frmMain.WindowState = FormWindowState.Minimized Then Return
+
                     MoveWindow(PuttyHandle, -SystemInformation.FrameBorderSize.Width, -(SystemInformation.CaptionHeight + SystemInformation.FrameBorderSize.Height), InterfaceControl.Width + (SystemInformation.FrameBorderSize.Width * 2), InterfaceControl.Height + SystemInformation.CaptionHeight + (SystemInformation.FrameBorderSize.Height * 2), True)
                 Catch ex As Exception
                     MessageCollector.AddMessage(Messages.MessageClass.ErrorMsg, My.Language.strPuttyResizeFailed & vbNewLine & ex.Message, True)
