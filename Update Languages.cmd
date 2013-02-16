67a6d60c624f908ac4257ffc5dcb3e9d07a14933
Riley McArdle <riley@glyff.net>
2013-02-15 20:44:21 -0600

    Updated translations with data from Crowdin.
    Added translations for Spanish (Argentina), Italian, Portuguese, Chinese (Simplified).

--- Update Languages.cmd
+++ Update Languages.cmd
@@ -27,14 +27,21 @@ echo.
 mkdir %TEMP_FOLDER%
 %RAR% x -o+ %ZIP_FILE% *.* %TEMP_FOLDER%
 
-%SORTRESX% %TEMP_FOLDER%\de\Language.de-DE.resx "%~dp0\mRemoteV1\Language\Language.de.resx"
-%SORTRESX% %TEMP_FOLDER%\en-US\Language..resx "%~dp0\mRemoteV1\Language\Language.en-US.resx"
-%SORTRESX% %TEMP_FOLDER%\es-ES\Language.es-ES.resx "%~dp0\mRemoteV1\Language\Language.es.resx"
-%SORTRESX% %TEMP_FOLDER%\fr\Language.fr-FR.resx "%~dp0\mRemoteV1\Language\Language.fr.resx"
-%SORTRESX% %TEMP_FOLDER%\it\Language.it-IT.resx "%~dp0\mRemoteV1\Language\Language.it.resx"
-%SORTRESX% %TEMP_FOLDER%\nl\Language.nl-NL.resx "%~dp0\mRemoteV1\Language\Language.nl.resx"
-%SORTRESX% %TEMP_FOLDER%\ru\Language.ru-RU.resx "%~dp0\mRemoteV1\Language\Language.ru.resx"
-%SORTRESX% %TEMP_FOLDER%\uk\Language.uk-UA.resx "%~dp0\mRemoteV1\Language\Language.uk.resx"
+%SORTRESX% %TEMP_FOLDER%\ar\Language.ar.resx "%~dp0\mRemoteV1\Language\Language.ar.resx"
+%SORTRESX% %TEMP_FOLDER%\de\Language.de.resx "%~dp0\mRemoteV1\Language\Language.de.resx"
+%SORTRESX% %TEMP_FOLDER%\en-US\Language.en.resx "%~dp0\mRemoteV1\Language\Language.en-US.resx"
+%SORTRESX% %TEMP_FOLDER%\es-AR\Language.es.resx "%~dp0\mRemoteV1\Language\Language.es-AR.resx"
+%SORTRESX% %TEMP_FOLDER%\es-ES\Language.es.resx "%~dp0\mRemoteV1\Language\Language.es.resx"
+%SORTRESX% %TEMP_FOLDER%\fr\Language.fr.resx "%~dp0\mRemoteV1\Language\Language.fr.resx"
+%SORTRESX% %TEMP_FOLDER%\hu\Language.hu.resx "%~dp0\mRemoteV1\Language\Language.hu.resx"
+%SORTRESX% %TEMP_FOLDER%\it\Language.it.resx "%~dp0\mRemoteV1\Language\Language.it.resx"
+%SORTRESX% %TEMP_FOLDER%\nl\Language.nl.resx "%~dp0\mRemoteV1\Language\Language.nl.resx"
+%SORTRESX% %TEMP_FOLDER%\pl\Language.pl.resx "%~dp0\mRemoteV1\Language\Language.pl.resx"
+%SORTRESX% %TEMP_FOLDER%\pt-BR\Language.pt.resx "%~dp0\mRemoteV1\Language\Language.pt-BR.resx"
+%SORTRESX% %TEMP_FOLDER%\pt-PT\Language.pt.resx "%~dp0\mRemoteV1\Language\Language.pt.resx"
+%SORTRESX% %TEMP_FOLDER%\ru\Language.ru.resx "%~dp0\mRemoteV1\Language\Language.ru.resx"
+%SORTRESX% %TEMP_FOLDER%\uk\Language.uk.resx "%~dp0\mRemoteV1\Language\Language.uk.resx"
+%SORTRESX% %TEMP_FOLDER%\zh-CN\Language.zh.resx "%~dp0\mRemoteV1\Language\Language.zh-CN.resx"
 
 rmdir /s /q %TEMP_FOLDER%
 
