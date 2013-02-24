94d901ec4ca26ea771f7353a6c925c233067b958
Riley McArdle <riley@glyff.net>
2013-02-24 15:40:19 -0600

    Increment version to 1.71.

--- mRemoteV1/My Project/AssemblyInfo.vb
+++ mRemoteV1/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.70.*")>
+<Assembly: AssemblyVersion("1.71.*")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
