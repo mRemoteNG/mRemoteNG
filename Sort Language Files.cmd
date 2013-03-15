d9ba8f81e21139b27a2cb3a669a786994aeadfa7
Riley McArdle <riley@glyff.net>
2013-03-14 22:01:14 -0500

    Create Sort Language Files.cmd.

---
+++ Sort Language Files.cmd
@@ -0,0 +1,26 @@
+@echo off
+
+set TEMP_FOLDER="%TEMP%\Translations.tmp"
+set SORTRESX="%~dp0\Tools\SortRESX.exe"
+
+echo.
+echo This script sorts the language files
+echo.
+
+choice /m "Would you like to continue?"
+
+echo.
+
+rmdir /s /q %TEMP_FOLDER%
+mkdir %TEMP_FOLDER%
+copy "%~dp0\mRemoteV1\Language\*.resx" %TEMP_FOLDER%
+
+pushd %TEMP_FOLDER%
+for %%f in (*) do echo %SORTRESX% %TEMP_FOLDER%\%%f "%~dp0\mRemoteV1\Language\%%f"
+popd
+
+rmdir /s /q %TEMP_FOLDER%
+
+echo Done.
+echo.
+pause
