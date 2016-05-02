84d849da4ccc243f5e82a8df82ee2034a28f427b
Kaim, Sean M <Sean.Kaim@ca.com>
2016-05-02 15:58:38 -0400

    Fix NullReferenceExceptions
    Introduced in commit 63e0cd198c0ac95801029ea4f50e542a172921d6
    Also performed some code cleanup

--- mRemoteV1/App/Info/SettingsFileInfo.cs
+++ mRemoteV1/App/Info/SettingsFileInfo.cs
@@ -1,13 +1,17 @@
 ﻿
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
 namespace mRemoteNG.App.Info
 {
     public class SettingsFileInfo
     {
-        #if !PORTABLE
-		public static readonly string SettingsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData) + "\\" + (new Microsoft.VisualBasic.ApplicationServices.WindowsFormsApplicationBase()).Info.ProductName;
-        #else
-        public static readonly string SettingsPath = (new Microsoft.VisualBasic.ApplicationServices.WindowsFormsApplicationBase()).Info.DirectoryPath;
-        #endif
+#if !PORTABLE
+		public static readonly string SettingsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData) + "\\" + Application.ProductName;
+#else
+        public static readonly string SettingsPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase) + "\\" + Application.ProductName;
+#endif
         public static readonly string LayoutFileName = "pnlLayout.xml";
         public static readonly string ExtAppsFilesName = "extApps.xml";
         public const string ThemesFileName = "Themes.xml";
