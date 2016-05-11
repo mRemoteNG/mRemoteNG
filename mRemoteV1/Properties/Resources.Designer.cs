f0c3395c00370169ea0e30c2b0d7ff130b6b3135
David Sparer <sparerd@gmail.com>
2016-05-10 18:06:33 -0600

    Updated namespace of Resources and Language classes. The namespace was set in the designer but the custom tool needed to be run again to update the actual files.

--- mRemoteV1/Properties/Resources.Designer.cs
+++ mRemoteV1/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace mRemoteNG.My {
+namespace mRemoteNG {
     using System;
     
     
