a32d85ab6ca9fbd79314b18d658cf27abcb85a58
David Sparer <sparerd@gmail.com>
2016-06-08 15:14:40 -0600

    Began building out the credential manager class, which will manage lists of credentials.

---
+++ mRemoteV1/Credential/CredentialManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mRemoteNG.Credential
+{
+    public class CredentialManager
+    {
+        private List<CredentialList> _credentailDataSources;
+
+
+        public CredentialManager()
+        {
+            _credentailDataSources = new List<CredentialList>();
+        }
+
+        public void AddDataSource(CredentialList Source)
+        {
+            _credentailDataSources.Add(Source);
+        }
+
+        public void RemoveDataSource(CredentialList Source)
+        {
+            // find source in list
+            // remove source
+        }
+    }
+}
