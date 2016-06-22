bc1d3c4c134a51526ccfb59f5b447f0045bd20b0
David Sparer <David.Sparer@pearson.com>
2016-06-22 15:23:36 -0600

    Began creating classes for saving/loading credential lists

---
+++ mRemoteV1/Config/Credentials/CredentialSaver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using mRemoteNG.Credential;
+
+namespace mRemoteNG.Config.Credentials
+{
+    public class CredentialSaver
+    {
+        private CredentialList _credentialList;
+
+        public CredentialSaver(CredentialList credentialList)
+        {
+            _credentialList = credentialList;
+        }
+
+        public void Save()
+        {
+            
+        }
+    }
+}
