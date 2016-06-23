503072429202ae2fda0ccfe1ee3bc2d1e700b8b3
David Sparer <David.Sparer@pearson.com>
2016-06-23 10:32:53 -0600

    Added a form for the credential editor

---
+++ mRemoteV1/UI/Forms/FrmCredentialEditor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mRemoteNG.UI.Forms
+{
+    public partial class FrmCredentialEditor : Form
+    {
+        public FrmCredentialEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
