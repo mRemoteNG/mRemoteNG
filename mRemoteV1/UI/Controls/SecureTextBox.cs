50e9673d2d447555fe27bdc23cc11795eb6717a6
David Sparer <David.Sparer@pearson.com>
2016-06-23 15:41:05 -0600

    Began creating a SecureTextBox control

---
+++ mRemoteV1/UI/Controls/SecureTextBox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace mRemoteNG.UI.Controls
+{
+    public partial class SecureTextBox : TextBox
+    {
+        public SecureTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
