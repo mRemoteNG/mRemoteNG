2b71688259c3f3189cf3225c95d5a64b550fde3d
David Sparer <David.Sparer@pearson.com>
2016-06-29 09:55:29 -0600

    Began adding UI tests for FrmCredentialManager

---
+++ mRemoteNGTests/UI/Forms/CredentialManagerTests.cs
@@ -0,0 +1,37 @@
+﻿using mRemoteNG.Credential;
+using mRemoteNG.UI.Forms;
+using NUnit.Framework;
+using NUnit.Extensions.Forms;
+
+namespace mRemoteNGTests.UI.Forms
+{
+    [TestFixture]
+    public class CredentialManagerTests
+    {
+        private FrmCredentialManager _credentialManager;
+        private CredentialList _credentialList;
+
+        [SetUp]
+        public void Setup()
+        {
+            _credentialList = new CredentialList();
+            _credentialManager = new FrmCredentialManager(_credentialList);
+            _credentialManager.Show();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _credentialManager.Dispose();
+            while (_credentialManager.Disposing) ;
+            _credentialManager = null;
+        }
+
+        [Test]
+        public void PasswordFormText()
+        {
+            var formTester = new FormTester(_credentialManager.Name);
+            Assert.That(formTester.Count, Is.EqualTo(1));
+        }
+    }
+}
