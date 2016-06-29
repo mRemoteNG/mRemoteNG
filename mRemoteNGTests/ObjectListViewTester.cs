a7932983dc0942fcc5521729ea0f9b30def015e7
David Sparer <David.Sparer@pearson.com>
2016-06-29 12:12:20 -0600

    Extended NUnitForms to support testing ObjectListViews

---
+++ mRemoteNGTests/ObjectListViewTester.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Forms;
+using BrightIdeasSoftware;
+
+namespace NUnit.Extensions.Forms
+{
+    public class ObjectListViewTester : ControlTester
+    {
+        public ObjectListViewTester(string name, string formName) : base(name, formName)
+        {
+        }
+
+        public ObjectListViewTester(string name, Form form) : base(name, form)
+        {
+        }
+
+        public ObjectListViewTester(string name) : base(name)
+        {
+        }
+
+        public ObjectListView Properties => (ObjectListView) Control;
+
+        public void Select(int index)
+        {
+            Properties.SelectedIndex = index;
+        }
+
+        public void Select(OLVListItem selectedOlvListItem)
+        {
+            Properties.SelectedItem = selectedOlvListItem;
+        }
+
+        public void SelectAll()
+        {
+            Properties.SelectAll();
+        }
+    }
+}
