17250772f7cd51930b22cecd288c1f5c79897502
David Sparer <David.Sparer@pearson.com>
2016-06-29 15:04:02 -0600

    Began creating tests for the FrmCredentialEditor

---
+++ mRemoteNGTests/UI/Forms/CredentialEditorTests.cs
@@ -0,0 +1,59 @@
+﻿using mRemoteNG.Credential;
+using mRemoteNG.UI.Forms;
+using NUnit.Framework;
+using NUnit.Extensions.Forms;
+
+namespace mRemoteNGTests.UI.Forms
+{
+    [TestFixture]
+    public class CredentialEditorTests
+    {
+        private FrmCredentialEditor _credentialEditorForm;
+
+        [TearDown]
+        public void TearDown()
+        {
+            _credentialEditorForm?.Dispose();
+        }
+
+        private void CreateCredentialEditorForm(CredentialInfo credentialInfo = null)
+        {
+            _credentialEditorForm = new FrmCredentialEditor(credentialInfo);
+            _credentialEditorForm.Show();
+        }
+
+        [Test]
+        public void CredentialEditorFormLoadsWhenNotGivenAnExistingCredentialInfo()
+        {
+            CreateCredentialEditorForm();
+            var credentialFormTester = new FormTester(_credentialEditorForm.Name);
+            Assert.That(credentialFormTester.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CredentialEditorFormLoadsWhenGivenAnExistingCredentialInfo()
+        {
+            CreateCredentialEditorForm(new CredentialInfo());
+            var credentialFormTester = new FormTester(_credentialEditorForm.Name);
+            Assert.That(credentialFormTester.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CancelClosesForm()
+        {
+            CreateCredentialEditorForm();
+            var cancelButton = new ButtonTester("btnCancel");
+            cancelButton.Click();
+            var credentialFormTester = new FormTester(_credentialEditorForm.Name);
+            Assert.That(credentialFormTester.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void EntryNameFieldIsEditable()
+        {
+            CreateCredentialEditorForm();
+            var entryNameField = new TextBoxTester("txtEntryName");
+            Assert.That(entryNameField.Properties.Enabled, Is.True);
+        }
+    }
+}
