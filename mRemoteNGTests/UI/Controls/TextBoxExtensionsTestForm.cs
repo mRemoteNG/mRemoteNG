960d923aad6ec5f5ccb253c9aa7d199aec7fc750
David Sparer <David.Sparer@pearson.com>
2016-06-30 14:41:42 -0600

    Added tests for the TextBoxExtensions

---
+++ mRemoteNGTests/UI/Controls/TextBoxExtensionsTestForm.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace mRemoteNGTests.UI.Controls
+{
+    public partial class TextBoxExtensionsTestForm : Form
+    {
+        public TextBoxExtensionsTestForm()
+        {
+            Application.EnableVisualStyles();
+            InitializeComponent();
+        }
+    }
+}
---
+++ mRemoteNGTests/UI/Controls/TextBoxExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using mRemoteNG.UI.Controls;
+using NUnit.Extensions.Forms;
+using NUnit.Framework;
+
+namespace mRemoteNGTests.UI.Controls
+{
+    [TestFixture]
+    public class TextBoxExtensionsTests
+    {
+        private TextBoxExtensionsTestForm _textBoxExtensionsTestForm;
+
+        [SetUp]
+        public void Setup()
+        {
+            _textBoxExtensionsTestForm = new TextBoxExtensionsTestForm();
+            _textBoxExtensionsTestForm.Show();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _textBoxExtensionsTestForm.Dispose();
+            while (_textBoxExtensionsTestForm.Disposing)
+            { }
+            _textBoxExtensionsTestForm = null;
+        }
+
+        [Test]
+        public void SetCueBannerSetsTheBannerText()
+        {
+            var text = "Type Here";
+            var textBox = new TextBoxTester("textBox1");
+            Assert.That(textBox.Properties.SetCueBannerText(text), Is.True);
+        }
+
+        [Test]
+        public void GetCueBannerReturnsCorrectValue()
+        {
+            var text = "Type Here";
+            var textBox = new TextBoxTester("textBox1");
+            textBox.Properties.SetCueBannerText(text);
+            Assert.That(textBox.Properties.GetCueBannerText(), Is.EqualTo(text));
+        }
+    }
+}
