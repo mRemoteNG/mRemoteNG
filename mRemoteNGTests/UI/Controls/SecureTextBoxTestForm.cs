077559a3843ae277847f8c6f7a23e9268e563b90
David Sparer <David.Sparer@pearson.com>
2016-07-01 11:46:47 -0600

    Created unit tests for helper for SecureTextBox

---
+++ mRemoteNGTests/UI/Controls/SecureTextBoxTestForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mRemoteNGTests.UI.Controls
+{
+    public partial class SecureTextBoxTestForm : Form
+    {
+        public SecureTextBoxTestForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ mRemoteNGTests/UI/Controls/SecureTextBoxTests.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Windows.Forms;
+using mRemoteNG.Security;
+using NUnit.Extensions.Forms;
+using NUnit.Framework;
+
+namespace mRemoteNGTests.UI.Controls
+{
+    public class SecureTextBoxTests
+    {
+        private SecureTextBoxTestForm _testForm;
+
+        [SetUp]
+        public void Setup()
+        {
+            _testForm = new SecureTextBoxTestForm();
+            _testForm.Show();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _testForm.Close();
+            while (_testForm.Disposing) { }
+            _testForm = null;
+        }
+
+        [Test]
+        public void TextboxInputGetsAddedToSecureString()
+        {
+            var textBox = new SecureTextBoxTester(_testForm.secureTextBox1.Name);
+            var textToSend = "abc123";
+            textBox.Properties.Text = textToSend;
+            Assert.That(textBox.Properties.SecureString.ConvertToUnsecureString(), Is.EqualTo(textToSend));
+        }
+    }
+}
