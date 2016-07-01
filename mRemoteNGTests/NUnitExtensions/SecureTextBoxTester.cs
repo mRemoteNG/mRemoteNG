45997bab1931ca6cc37d6f97b4f435163b1a1951
David Sparer <David.Sparer@pearson.com>
2016-07-01 10:28:37 -0600

    Created an NUnitForms test helper for SecureTextBox. This object still needs to be made more testable

---
+++ mRemoteNGTests/NUnitExtensions/SecureTextBoxTester.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+using mRemoteNG.UI.Controls;
+
+// ReSharper disable once CheckNamespace
+namespace NUnit.Extensions.Forms
+{
+    public class SecureTextBoxTester : ControlTester
+    {
+        public SecureTextBoxTester(string name, string formName) : base(name, formName)
+        {
+        }
+
+        public SecureTextBoxTester(string name, Form form) : base(name, form)
+        {
+        }
+
+        public SecureTextBoxTester(string name) : base(name)
+        {
+        }
+
+        public SecureTextBox Properties => (SecureTextBox)Control;
+    }
+}
