957ac494fc249ee50d340062c989145fefbedce3
David Sparer <sparerd@gmail.com>
2016-07-04 13:04:35 -0600

    Add serialization attributes to CredentialInfo

--- mRemoteV1/Credential/CredentialInfo.cs
+++ mRemoteV1/Credential/CredentialInfo.cs
@@ -2,22 +2,27 @@ using mRemoteNG.Tools;
 using System;
 using System.ComponentModel;
 using System.Security;
+using System.Xml.Serialization;
 
 namespace mRemoteNG.Credential
 {
-	public class CredentialInfo
+    [Serializable()]
+    public class CredentialInfo
 	{
-	    public string Uuid { get; set; } = Guid.NewGuid().ToString();
+        [XmlElement("Uuid")]
+        public string Uuid { get; set; } = Guid.NewGuid().ToString();
 
 	    [LocalizedAttributes.LocalizedCategory("strCategoryDisplay", 1),
 	     Browsable(true),
-	     LocalizedAttributes.LocalizedDisplayName("strPropertyNameName"),
+         XmlElement("Name"),
+         LocalizedAttributes.LocalizedDisplayName("strPropertyNameName"),
 	     LocalizedAttributes.LocalizedDescription("strPropertyDescriptionName")]
 	    public string Name { get; set; } = Language.strNewCredential;
 		
 
         [LocalizedAttributes.LocalizedCategory("strCategoryDisplay", 1),
             Browsable(true),
+         XmlElement("Description"),
             LocalizedAttributes.LocalizedDisplayName("strPropertyNameDescription"),
             LocalizedAttributes.LocalizedDescription("strPropertyDescriptionDescription")]
         public string Description { get; set; }
@@ -25,6 +30,7 @@ namespace mRemoteNG.Credential
 
         [LocalizedAttributes.LocalizedCategory("strCategoryCredentials", 2), 
             Browsable(true),
+         XmlElement("Username"),
             LocalizedAttributes.LocalizedDisplayName("strPropertyNameUsername"),
             LocalizedAttributes.LocalizedDescription("strPropertyDescriptionUsername")]
         public string Username { get; set; }
@@ -32,6 +38,7 @@ namespace mRemoteNG.Credential
 
         [LocalizedAttributes.LocalizedCategory("strCategoryCredentials", 2), 
             Browsable(true),
+         XmlElement("Password"),
             LocalizedAttributes.LocalizedDisplayName("strPropertyNamePassword"),
             LocalizedAttributes.LocalizedDescription("strPropertyDescriptionPassword"), 
             PasswordPropertyText(true)]
@@ -40,13 +47,15 @@ namespace mRemoteNG.Credential
 		
         [LocalizedAttributes.LocalizedCategory("strCategoryCredentials", 2),
             Browsable(true),
+         XmlElement("Domain"),
             LocalizedAttributes.LocalizedDisplayName("strPropertyNameDomain"),
             LocalizedAttributes.LocalizedDescription("strPropertyDescriptionDomain")]
         public string Domain { get; set; }
 
 	    [LocalizedAttributes.LocalizedCategory("strCategoryCredentials", 2),
 	     Browsable(true),
-	     LocalizedAttributes.LocalizedDisplayName("strPropertyNameDomain"),
+         XmlElement("CredentialSource"),
+         LocalizedAttributes.LocalizedDisplayName("strPropertyNameDomain"),
 	     LocalizedAttributes.LocalizedDescription("strPropertyDescriptionDomain")]
 	    public CredentialSource CredentialSource { get; set; } = default(CredentialSource);
 
