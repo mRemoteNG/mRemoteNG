197a92c46d2c9bd5a45304c7a25dab11940571dd
David Sparer <sparerd@gmail.com>
2016-07-04 13:05:40 -0600

    Began creating the class for saving credentialinfo objects

--- mRemoteV1/Config/Credentials/CredentialSaver.cs
+++ mRemoteV1/Config/Credentials/CredentialSaver.cs
@@ -1,23 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
 using mRemoteNG.Credential;
 
 namespace mRemoteNG.Config.Credentials
 {
     public class CredentialSaver
     {
-        private CredentialList _credentialList;
+        private string _credentialFilePath;
 
-        public CredentialSaver(CredentialList credentialList)
+        public CredentialSaver(string credentialFilePath)
         {
-            _credentialList = credentialList;
+            _credentialFilePath = credentialFilePath;
         }
 
-        public void Save()
+        public void Save(CredentialList credentialList)
         {
-            
+            var xmlDoc = BuildXmlDocument(credentialList);
+            xmlDoc.Save(_credentialFilePath);
+        }
+
+        private XmlDocument BuildXmlDocument(IEnumerable credentialList)
+        {
+            var xmlDoc = new XmlDocument();
+            var xmlSerializer = new XmlSerializer(typeof(CredentialInfo));
+            using (var xmlStream = new MemoryStream())
+            {
+                foreach (var credential in credentialList)
+                {
+                    xmlSerializer.Serialize(xmlStream, credential);
+                }
+                
+                xmlStream.Position = 0;
+                xmlDoc.Load(xmlStream);
+            }
+            return xmlDoc;
         }
     }
 }
