73c50837d8c275571588e706ec47de7d004a86d4
David Sparer <David.Sparer@pearson.com>
2016-07-13 12:51:21 -0600

    Created class for building and using encrypted secure strings

---
+++ mRemoteNGTests/Security/EncryptedSecureStringTests.cs
@@ -0,0 +1,45 @@
+﻿using mRemoteNG.Security;
+using NUnit.Framework;
+
+namespace mRemoteNGTests.Security
+{
+    [TestFixture()]
+    public class EncryptedSecureStringTests
+    {
+        private readonly string _clearTextData = "MySecureData";
+
+        [Test]
+        public void CanCreateEncryptedSecureString()
+        {
+            Assert.That(new EncryptedSecureString(), Is.InstanceOf<EncryptedSecureString>());
+        }
+
+        [Test]
+        public void CanAssignStringValue()
+        {
+            var encryptedSecString = new EncryptedSecureString();
+            TestDelegate testDelegate = () => encryptedSecString.SetValue(_clearTextData);
+            Assert.DoesNotThrow(testDelegate);
+        }
+
+        [Test]
+        public void EncryptedValueIsSameAsOriginalValue()
+        {
+            var encryptedSecString = new EncryptedSecureString();
+            encryptedSecString.SetValue(_clearTextData);
+            var decryptedData = encryptedSecString.GetClearTextValue();
+            Assert.That(decryptedData, Is.EqualTo(_clearTextData));
+        }
+
+        [Test]
+        public void CreatingMultipleEncryptedSecureStrings()
+        {
+            var encString1 = new EncryptedSecureString();
+            encString1.SetValue(_clearTextData);
+            var encString2 = new EncryptedSecureString();
+            encString2.SetValue("somevalue");
+            var decryptedString = encString1.GetClearTextValue();
+            Assert.That(decryptedString, Is.EqualTo(_clearTextData));
+        }
+    }
+}
---
+++ mRemoteV1/Security/EncryptedSecureString.cs
@@ -0,0 +1,54 @@
+﻿using System.Security;
+using mRemoteNG.Security.SymmetricEncryption;
+using Org.BouncyCastle.Security;
+
+namespace mRemoteNG.Security
+{
+    public class EncryptedSecureString
+    {
+        private static SecureString _machineKey;
+        private SecureString _secureString;
+        private readonly ICryptographyProvider _cryptographyProvider;
+
+        private static SecureString MachineKey => _machineKey ?? (_machineKey = GenerateNewMachineKey(32));
+
+        public EncryptedSecureString()
+        {
+            _secureString = new SecureString();
+            _cryptographyProvider = new AeadCryptographyProvider();
+        }
+
+        public EncryptedSecureString(ICryptographyProvider cryptographyProvider)
+        {
+            _secureString = new SecureString();
+            _cryptographyProvider = cryptographyProvider;
+        }
+
+        public string GetClearTextValue()
+        {
+            var encryptedText = _secureString.ConvertToUnsecureString();
+            var clearText = _cryptographyProvider.Decrypt(encryptedText, MachineKey);
+            return clearText;
+        }
+
+        public void SetValue(string value)
+        {
+            var cipherText = _cryptographyProvider.Encrypt(value, MachineKey);
+            _secureString = cipherText.ConvertToSecureString();
+        }
+
+        private static SecureString GenerateNewMachineKey(int keySize)
+        {
+            var random = new SecureRandom();
+            random.SetSeed(random.GenerateSeed(128));
+
+            var machineKeyString = "";
+            for (var x = 0; x < keySize; x++)
+            {
+                machineKeyString += (char)random.Next(33, 126);
+            }
+            
+            return machineKeyString.ConvertToSecureString();
+        }
+    }
+}
