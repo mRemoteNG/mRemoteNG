a2054b458bb0890285040a448d189814e0188248
David Sparer <David.Sparer@pearson.com>
2016-07-13 08:16:06 -0600

    Code cleanup on the legacy crypt class

--- mRemoteV1/Security/Crypt.cs
+++ mRemoteV1/Security/Crypt.cs
@@ -3,43 +3,43 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using mRemoteNG.App;
-using mRemoteNG.My;
+using mRemoteNG.Messages;
 
 
 namespace mRemoteNG.Security
 {
 	public class Crypt
 	{
-		public static string Encrypt(string StrToEncrypt, string StrSecret)
+		public static string Encrypt(string strToEncrypt, string strSecret)
 		{
-			if (StrToEncrypt == "" || StrSecret == "")
+			if (strToEncrypt == "" || strSecret == "")
 			{
-				return StrToEncrypt;
+				return strToEncrypt;
 			}
-				
+			
 			try
 			{
-				RijndaelManaged rd = new RijndaelManaged();
-					
-				MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
-				byte[] key = md5.ComputeHash(Encoding.UTF8.GetBytes(StrSecret));
+				var rd = new RijndaelManaged();
+
+                var md5 = new MD5CryptoServiceProvider();
+                var key = md5.ComputeHash(Encoding.UTF8.GetBytes(strSecret));
 					
 				md5.Clear();
 				rd.Key = key;
 				rd.GenerateIV();
-					
-				byte[] iv = rd.IV;
-				MemoryStream ms = new MemoryStream();
+
+                var iv = rd.IV;
+                var ms = new MemoryStream();
 					
 				ms.Write(iv, 0, iv.Length);
-					
-				CryptoStream cs = new CryptoStream(ms, rd.CreateEncryptor(), CryptoStreamMode.Write);
-				byte[] data = System.Text.Encoding.UTF8.GetBytes(StrToEncrypt);
+
+                var cs = new CryptoStream(ms, rd.CreateEncryptor(), CryptoStreamMode.Write);
+                var data = Encoding.UTF8.GetBytes(strToEncrypt);
 					
 				cs.Write(data, 0, data.Length);
 				cs.FlushFinalBlock();
-					
-				byte[] encdata = ms.ToArray();
+
+                var encdata = ms.ToArray();
 				cs.Close();
 				rd.Clear();
 					
@@ -47,64 +47,49 @@ namespace mRemoteNG.Security
 			}
 			catch (Exception ex)
 			{
-				Runtime.MessageCollector.AddMessage(Messages.MessageClass.ErrorMsg, string.Format(Language.strErrorEncryptionFailed, ex.Message));
+				Runtime.MessageCollector.AddMessage(MessageClass.ErrorMsg, string.Format(Language.strErrorEncryptionFailed, ex.Message));
 			}
 				
-			return StrToEncrypt;
+			return strToEncrypt;
 		}
 			
 		public static string Decrypt(string ciphertextBase64, string password)
 		{
 			if (string.IsNullOrEmpty(ciphertextBase64) || string.IsNullOrEmpty(password))
-			{
 				return ciphertextBase64;
-			}
 				
 			try
 			{
-				string plaintext = "";
-					
-				using (RijndaelManaged rijndaelManaged = new RijndaelManaged())
-				{
-					using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
-					{
-						byte[] key = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
-						rijndaelManaged.Key = key;
-					}
-						
-						
-					byte[] ciphertext = Convert.FromBase64String(ciphertextBase64);
-						
-					using (MemoryStream memoryStream = new MemoryStream(ciphertext))
-					{
-						const int ivLength = 16;
-						byte[] iv = new byte[ivLength - 1 + 1];
-						memoryStream.Read(iv, 0, ivLength);
-						rijndaelManaged.IV = iv;
-							
-						using (CryptoStream cryptoStream = new CryptoStream(memoryStream, rijndaelManaged.CreateDecryptor(), CryptoStreamMode.Read))
-						{
-							using (StreamReader streamReader = new StreamReader(cryptoStream, System.Text.Encoding.UTF8, true))
-							{
-								plaintext = streamReader.ReadToEnd();
-							}
-								
-							rijndaelManaged.Clear();
-						} // cryptoStream
-							
-					} // memoryStream
-						
-				} // rijndaelManaged
-					
-					
-				return plaintext;
+                var plaintext = "";
+
+                using (var rijndaelManaged = new RijndaelManaged())
+                using (var md5 = new MD5CryptoServiceProvider())
+                {
+                    var key = md5.ComputeHash(Encoding.UTF8.GetBytes(password));
+                    rijndaelManaged.Key = key;
+                    var ciphertext = Convert.FromBase64String(ciphertextBase64);
+
+                    using (var memoryStream = new MemoryStream(ciphertext))
+                    using (var cryptoStream = new CryptoStream(memoryStream, rijndaelManaged.CreateDecryptor(), CryptoStreamMode.Read))
+                    using (var streamReader = new StreamReader(cryptoStream, Encoding.UTF8, true))
+                    {
+                        const int ivLength = 16;
+                        var iv = new byte[ivLength - 1 + 1];
+                        memoryStream.Read(iv, 0, ivLength);
+                        rijndaelManaged.IV = iv;
+                        plaintext = streamReader.ReadToEnd();
+                        rijndaelManaged.Clear();
+                    }
+                }
+
+			    return plaintext;
 			}
 			catch (Exception ex)
 			{
 				// Ignore CryptographicException "Padding is invalid and cannot be removed." when password is incorrect.
 				if (!(ex is CryptographicException))
 				{
-					Runtime.MessageCollector.AddMessage(Messages.MessageClass.ErrorMsg, string.Format(Language.strErrorDecryptionFailed, ex.Message));
+					Runtime.MessageCollector.AddMessage(MessageClass.ErrorMsg, string.Format(Language.strErrorDecryptionFailed, ex.Message));
 				}
 			}
 				
