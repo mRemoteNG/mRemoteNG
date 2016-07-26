cc96e2b3eb5592dbe76f9754a64dc6f28789d5ea
David Sparer <David.Sparer@pearson.com>
2016-07-26 11:30:52 -0600

    Removed more unused code

--- mRemoteV1/Config/Connections/ConnectionsLoader.cs
+++ mRemoteV1/Config/Connections/ConnectionsLoader.cs
@@ -1,5 +1,4 @@
 using System.Windows.Forms;
-using System.Xml;
 using mRemoteNG.Connection;
 using mRemoteNG.Container;
 using mRemoteNG.UI.Forms;
@@ -8,10 +7,7 @@ using mRemoteNG.UI.Forms;
 namespace mRemoteNG.Config.Connections
 {
 	public class ConnectionsLoader
-	{
-		private XmlDocument _xmlDocument;
-		private double _confVersion;
-		
+	{		
         public bool UseSql { get; set; }
 	    public string SqlHost { get; set; }
 	    public string SqlDatabaseName { get; set; }
