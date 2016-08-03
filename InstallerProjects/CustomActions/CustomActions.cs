fdb3cecf8e7e3172dd5ed193be8ffb6179b7ce9b
David Sparer <David.Sparer@pearson.com>
2016-08-03 12:07:13 -0600

    Refactored CustomActions to extract a class

--- InstallerProjects/CustomActions/CustomActions.cs
+++ InstallerProjects/CustomActions/CustomActions.cs
@@ -5,29 +5,10 @@ namespace CustomActions
     public class CustomActions
     {
         [CustomAction]
-        public static ActionResult IsMinimumRdpVersionInstalleds(Session session)
+        public static ActionResult IsMinimumRdpVersionInstalled(Session session)
         {
             var rdpVersionChecker = new RdpVersionChecker(session);
             rdpVersionChecker.Execute();
-            return ActionResult.Success;
-        }
-
-        [CustomAction]
-        public static ActionResult IsMinimumRdpVersionInstalled(Session session)
-        {
-            const string MinimumVersionInstalledReturnVar = "MINIMUM_RDP_VERSION_INSTALLED";
-            const string MinimumRdpKbVariable = "MINIMUM_RDP_KB";
-            session.Log("Begin IsMinimumRdpVersionInstalled");
-            var minimumKb = session[MinimumRdpKbVariable];
-            session.Log("Checking if '{0}' is installed", minimumKb);
-            var updateGatherer = new InstalledWindowsUpdateGatherer();
-            var isUpdateInstalled = updateGatherer.IsUpdateInstalled(minimumKb);
-            session.Log("KB is installed = '{0}'", isUpdateInstalled);
-            var updateInstalledVal = isUpdateInstalled ? "1" : "0";
-            session[MinimumVersionInstalledReturnVar] = updateInstalledVal;
-            session.Log($"Set property '{MinimumVersionInstalledReturnVar}' to '{updateInstalledVal}'");
-            session.Log("End IsMinimumRdpVersionInstalled");
-
             return ActionResult.Success;
         }
 
