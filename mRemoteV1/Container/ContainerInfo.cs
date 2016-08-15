41127e0e382aea4ab95db6c8a271c28fd7c6f06b
David Sparer <David.Sparer@pearson.com>
2016-08-15 12:02:29 -0600

    Added collection management functions to ContainerInfo (Add, AddRange, Remove, RemoveRange). Added unit tests

--- mRemoteV1/Container/ContainerInfo.cs
+++ mRemoteV1/Container/ContainerInfo.cs
@@ -19,12 +19,40 @@ namespace mRemoteNG.Container
             IsContainer = true;
         }
 
+        public void Add(ConnectionInfo newChildItem)
+        {
+            newChildItem.Parent = this;
+            Children.Add(newChildItem);
+        }
+
+        public void AddRange(IEnumerable<ConnectionInfo> newChildren)
+        {
+            foreach (var child in newChildren)
+            {
+                Add(child);
+            }
+        }
+
+        public void Remove(ConnectionInfo removalTarget)
+        {
+            removalTarget.Parent = null;
+            Children.Remove(removalTarget);
+        }
+
+        public void RemoveRange(IEnumerable<ConnectionInfo> removalTargets)
+        {
+            foreach (var child in removalTargets)
+            {
+                Remove(child);
+            }
+        }
+
         public new ContainerInfo Copy()
 		{
 			return (ContainerInfo)MemberwiseClone();
 		}
 			
-		public void SetDefaults()
+		private void SetDefaults()
 		{
             IsExpanded = true;
 		}
