4692b20a746de2ba2372de4bd07d8d5b2305a7b8
Sean Kaim <sean.kaim@gmail.com>
2016-08-26 17:07:59 -0400

    more code clean up

--- mRemoteV1/App/Info/SettingsFileInfo.cs
+++ mRemoteV1/App/Info/SettingsFileInfo.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Reflection;
-using System.Windows.Forms;
 
 namespace mRemoteNG.App.Info
 {
-    public class SettingsFileInfo
+    public static class SettingsFileInfo
     {
         //public static readonly string exe = Assembly.GetExecutingAssembly().GetName().CodeBase;
         public static readonly string exePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
