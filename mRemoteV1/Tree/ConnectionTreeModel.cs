8bf500414707822a6c3c07461ba6aed8e9f5405e
David Sparer <David.Sparer@pearson.com>
2016-09-07 12:49:41 -0600

    Fixed nullref error when attempting to delete a node when nothing is selected

--- mRemoteV1/Tree/ConnectionTreeModel.cs
+++ mRemoteV1/Tree/ConnectionTreeModel.cs
@@ -35,7 +35,8 @@ namespace mRemoteNG.Tree
             if (connectionInfo is RootNodeInfo)
                 return;
             
-            connectionInfo.Dispose();
+            connectionInfo?.Dispose();
+        }
         }
     }
 }
