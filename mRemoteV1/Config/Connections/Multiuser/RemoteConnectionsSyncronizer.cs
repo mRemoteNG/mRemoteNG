2df93bea24b7613f49ad9bd5a2b269a7bee29971
David Sparer <David.Sparer@pearson.com>
2016-10-03 10:28:08 -0600

    Reduced some complexity by merging the PeriodicConnectionsUpdateChecker with the RemoteConnectionsSyncronizer since they did nearly the same thing.

--- mRemoteV1/Config/Connections/Multiuser/RemoteConnectionsSyncronizer.cs
+++ mRemoteV1/Config/Connections/Multiuser/RemoteConnectionsSyncronizer.cs
@@ -1,21 +1,35 @@
 ﻿using System;
+using System.Timers;
 using mRemoteNG.App;
 
 
 namespace mRemoteNG.Config.Connections
 {
-    public class RemoteConnectionsSyncronizer : IDisposable
+    public class RemoteConnectionsSyncronizer : IConnectionsUpdateChecker
     {
-        private readonly PeriodicConnectionsUpdateChecker _periodicConnectionsUpdateChecker;
+        private readonly Timer _updateTimer;
+        private readonly IConnectionsUpdateChecker _updateChecker;
         private readonly ConnectionsLoader _connectionsLoader;
         private readonly ConnectionsSaver _connectionsSaver;
 
+        public double TimerIntervalInMilliseconds => _updateTimer.Interval;
+
         public RemoteConnectionsSyncronizer(IConnectionsUpdateChecker updateChecker)
         {
-            _periodicConnectionsUpdateChecker = new PeriodicConnectionsUpdateChecker(updateChecker);
+            _updateChecker = updateChecker;
+            _updateTimer = new Timer(3000);
             _connectionsLoader = new ConnectionsLoader { UseDatabase = mRemoteNG.Settings.Default.UseSQLServer };
             _connectionsSaver = new ConnectionsSaver { SaveFormat = ConnectionsSaver.Format.SQL };
-            _periodicConnectionsUpdateChecker.ConnectionsUpdateAvailable += Load;
+            SetEventListeners();
+        }
+
+        private void SetEventListeners()
+        {
+            _updateChecker.UpdateCheckStarted += OnUpdateCheckStarted;
+            _updateChecker.UpdateCheckFinished += OnUpdateCheckFinished;
+            _updateChecker.ConnectionsUpdateAvailable += (sender, args) => ConnectionsUpdateAvailable?.Invoke(sender, args);
+            _updateTimer.Elapsed += (sender, args) => _updateChecker.IsUpdateAvailableAsync();
+            ConnectionsUpdateAvailable += Load;
         }
 
         public void Load()
@@ -34,9 +48,27 @@ namespace mRemoteNG.Config.Connections
             _connectionsSaver.SaveConnections();
         }
 
-        public void Enable() => _periodicConnectionsUpdateChecker.Enable();
-        public void Disable() => _periodicConnectionsUpdateChecker.Disable();
+        public void Enable() => _updateTimer.Start();
+        public void Disable() => _updateTimer.Stop();
+        public bool IsUpdateAvailable() => _updateChecker.IsUpdateAvailable();
+        public void IsUpdateAvailableAsync() => _updateChecker.IsUpdateAvailableAsync();
 
+
+        private void OnUpdateCheckStarted(object sender, EventArgs eventArgs)
+        {
+            _updateTimer.Stop();
+            UpdateCheckStarted?.Invoke(sender, eventArgs);
+        }
+
+        private void OnUpdateCheckFinished(object sender, ConnectionsUpdateCheckFinishedEventArgs eventArgs)
+        {
+            _updateTimer.Start();
+            UpdateCheckFinished?.Invoke(sender, eventArgs);
+        }
+
+        public event EventHandler UpdateCheckStarted;
+        public event UpdateCheckFinishedEventHandler UpdateCheckFinished;
+        public event ConnectionsUpdateAvailableEventHandler ConnectionsUpdateAvailable;
 
         ~RemoteConnectionsSyncronizer()
         {
@@ -50,7 +82,8 @@ namespace mRemoteNG.Config.Connections
         private void Dispose(bool itIsSafeToAlsoFreeManagedObjects)
         {
             if (!itIsSafeToAlsoFreeManagedObjects) return;
-            _periodicConnectionsUpdateChecker.Dispose();
+            _updateTimer.Dispose();
+            _updateChecker.Dispose();
         }
     }
 }
