960de69c2060c6fa46623e17d08283fb20bbc24f
David Sparer <David.Sparer@pearson.com>
2016-10-04 10:18:49 -0600

    Added note regarding a minor efficiency bug to investigate

--- mRemoteV1/UI/Window/ConnectionTreeWindow.cs
+++ mRemoteV1/UI/Window/ConnectionTreeWindow.cs
@@ -239,6 +239,7 @@ namespace mRemoteNG.UI.Window
 
         private void HandleCollectionPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
         {
+            //TODO for some reason property changed events are getting triggered twice for each changed property. should be just once. cant find source of duplication
             if (propertyChangedEventArgs.PropertyName != "Name") return;
             var senderAsConnectionInfo = sender as ConnectionInfo;
             if (senderAsConnectionInfo != null)
