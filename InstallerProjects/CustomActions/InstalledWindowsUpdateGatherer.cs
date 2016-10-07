9c5a2c0196f9e84f2b40aec5ff6f6bd7ed619a96
David Sparer <David.Sparer@pearson.com>
2016-10-07 10:26:21 -0600

    Added more efficient support for checking if atleast one of a number of KBs are installed.

--- InstallerProjects/CustomActions/InstalledWindowsUpdateGatherer.cs
+++ InstallerProjects/CustomActions/InstalledWindowsUpdateGatherer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Management;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace CustomActions
 {
@@ -42,12 +43,33 @@ namespace CustomActions
 
         public bool IsUpdateInstalled(string kb)
         {
+            return IsUpdateInstalled(new[] {kb});
+        }
+
+        public bool IsUpdateInstalled(IEnumerable<string> kbList)
+        {
             var updateIsInstalled = false;
-            var query = $"SELECT HotFixID FROM Win32_QuickFixEngineering WHERE HotFixID='{kb}'";
+            var whereClause = BuildWhereClauseFromKbList(kbList);
+            if (whereClause == "") return false;
+            var query = $"SELECT HotFixID FROM Win32_QuickFixEngineering WHERE {whereClause}";
             var searcher = new ManagementObjectSearcher(_managementScope, new ObjectQuery(query));
             if (searcher.Get().Count > 0)
                 updateIsInstalled = true;
             return updateIsInstalled;
+        }
+
+        private string BuildWhereClauseFromKbList(IEnumerable<string> kbList)
+        {
+            var whereClause = "";
+            var counter = 0;
+            foreach (var kb in kbList)
+            {
+                if (counter > 0)
+                    whereClause += " OR ";
+                whereClause += $"HotFixID='{kb}'";
+                counter++;
+            }
+            return whereClause;
         }
     }
 }
