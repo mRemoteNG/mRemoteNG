ba878c7587c7e3825b5dbb0f9a91a0251a3028ca
David Sparer <David.Sparer@pearson.com>
2016-10-24 10:51:59 -0600

    Moved some of the responsibility of the default encryption password into the RootNodeInfo class. This simplifies serialization a bit, though may not be the best place for storing encryption passwords

---
+++ mRemoteNGTests/Tree/RootNodeInfoTests.cs
@@ -0,0 +1,35 @@
+﻿using mRemoteNG.Security;
+using mRemoteNG.Tree.Root;
+using NUnit.Framework;
+
+
+namespace mRemoteNGTests.Tree
+{
+    public class RootNodeInfoTests
+    {
+        private RootNodeInfo _rootNodeInfo;
+
+        [SetUp]
+        public void Setup()
+        {
+            _rootNodeInfo = new RootNodeInfo(RootNodeType.Connection);
+        }
+
+        [Test]
+        public void DefaultPasswordReturnsExpectedValue()
+        {
+            var defaultPassword = _rootNodeInfo.DefaultPassword.ConvertToUnsecureString();
+            Assert.That(defaultPassword, Is.EqualTo("mR3m"));
+        }
+
+        [TestCase("a", true)]
+        [TestCase("mR3m", true)]
+        [TestCase("", false)]
+        [TestCase(null, false)]
+        public void PasswordPropertyReflectsWhetherACustomPasswordIsInUse(string password, bool expected)
+        {
+            _rootNodeInfo.PasswordString = password;
+            Assert.That(_rootNodeInfo.Password, Is.EqualTo(expected));
+        }
+    }
+}
