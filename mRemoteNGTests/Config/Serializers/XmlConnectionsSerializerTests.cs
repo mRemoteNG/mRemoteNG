6ca5f5e87791230b27ce7c026278bbc6c70c1e77
David Sparer <David.Sparer@pearson.com>
2016-10-25 08:14:49 -0600

    minor cleanup

--- mRemoteNGTests/Config/Serializers/XmlConnectionsSerializerTests.cs
+++ mRemoteNGTests/Config/Serializers/XmlConnectionsSerializerTests.cs
@@ -49,22 +49,22 @@ namespace mRemoteNGTests.Config.Serializers
              */
             var connectionTreeModel = new ConnectionTreeModel();
             var rootNode = new RootNodeInfo(RootNodeType.Connection);
-            var _folder1 = new ContainerInfo { Name = "folder1" };
-            var _folder2 = new ContainerInfo { Name = "folder2" };
-            var _folder3 = new ContainerInfo { Name = "folder3" };
-            var _con0 = new ConnectionInfo { Name = "con0" };
-            var _con1 = new ConnectionInfo { Name = "con1" };
-            var _con2 = new ConnectionInfo { Name = "con2" };
-            var _con3 = new ConnectionInfo { Name = "con3" };
-            var _con4 = new ConnectionInfo { Name = "con4" };
-            rootNode.AddChild(_folder1);
-            rootNode.AddChild(_folder2);
-            rootNode.AddChild(_con0);
-            _folder1.AddChild(_con1);
-            _folder2.AddChild(_con2);
-            _folder2.AddChild(_folder3);
-            _folder3.AddChild(_con3);
-            _folder3.AddChild(_con4);
+            var folder1 = new ContainerInfo { Name = "folder1" };
+            var folder2 = new ContainerInfo { Name = "folder2" };
+            var folder3 = new ContainerInfo { Name = "folder3" };
+            var con0 = new ConnectionInfo { Name = "con0" };
+            var con1 = new ConnectionInfo { Name = "con1" };
+            var con2 = new ConnectionInfo { Name = "con2" };
+            var con3 = new ConnectionInfo { Name = "con3" };
+            var con4 = new ConnectionInfo { Name = "con4" };
+            rootNode.AddChild(folder1);
+            rootNode.AddChild(folder2);
+            rootNode.AddChild(con0);
+            folder1.AddChild(con1);
+            folder2.AddChild(con2);
+            folder2.AddChild(folder3);
+            folder3.AddChild(con3);
+            folder3.AddChild(con4);
             connectionTreeModel.AddRootNode(rootNode);
             return connectionTreeModel;
         }
