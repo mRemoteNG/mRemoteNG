a03ce5bf7b6d8b982f108b8e388504bebcc806ef
David Sparer <David.Sparer@pearson.com>
2016-10-28 10:32:53 -0600

    Created minimalistic class to encapsulate the credential list (repository)

---
+++ mRemoteNGTests/Credential/CredentialRepositoryTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using mRemoteNG.Credential;
+using NUnit.Framework;
+
+
+namespace mRemoteNGTests.Credential
+{
+    public class CredentialRepositoryTests
+    {
+        private CredentialRepository _credentialRepository;
+
+        [SetUp]
+        public void Setup()
+        {
+            _credentialRepository = new CredentialRepository();
+        }
+
+        [Test]
+        public void IsAList()
+        {
+            Assert.That(_credentialRepository, Is.AssignableTo<IList<ICredential>>());
+        }
+    }
+}
---
+++ mRemoteV1/Credential/CredentialRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+
+namespace mRemoteNG.Credential
+{
+    public class CredentialRepository : List<ICredential>
+    {
+    }
+}
