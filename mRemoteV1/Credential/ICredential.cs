6aa363830b51b1acc3f74d37de307b9317093dbe
David Sparer <David.Sparer@pearson.com>
2016-10-28 11:52:11 -0600

    Minor formatting fix

--- mRemoteV1/Credential/ICredential.cs
+++ mRemoteV1/Credential/ICredential.cs
@@ -7,8 +7,11 @@ namespace mRemoteNG.Credential
     public interface ICredential
     {
         Guid UniqueId { get; }
+
         string Username { get; set; }
+
         SecureString Password { get; set; }
+
         string Domain { get; set; }
     }
 }
