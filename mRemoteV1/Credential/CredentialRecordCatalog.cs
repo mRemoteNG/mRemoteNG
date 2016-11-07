656e3ff0228f947eaa6356081dc82fd2bceadc71
David Sparer <David.Sparer@pearson.com>
2016-11-07 15:33:06 -0700

    Began fleshing out the CredentialRecordCatalog which will be a master list of all ICredentialRecords. Will probably be refactoring this a bit more.

---
+++ mRemoteV1/Credential/CredentialRecordCatalog.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace mRemoteNG.Credential
+{
+    public class CredentialRecordCatalog
+    {
+        public List<ICredentialRecord> CredentialRecords { get; } = new List<ICredentialRecord>();
+
+        public void AddCredentialList(ICredentialList credentialList)
+        {
+            CredentialRecords.AddRange(credentialList);
+        }
+
+        public void RemoveCredentialList(ICredentialList credentialList)
+        {
+            foreach (var credentialToRemove in credentialList)
+            {
+                if (!CredentialRecords.Contains(credentialToRemove))
+                    continue;
+                CredentialRecords.Remove(credentialToRemove);
+            }
+        }
+    }
+}
