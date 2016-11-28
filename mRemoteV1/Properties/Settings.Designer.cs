d20e1a6e9c07d92abc98bfdd20f74808474b74c6
David Sparer <David.Sparer@pearson.com>
2016-11-28 13:01:31 -0700

    added Turkish translation provided by forum user rizaemet

--- mRemoteV1/Properties/Settings.Designer.cs
+++ mRemoteV1/Properties/Settings.Designer.cs
@@ -1873,7 +1873,8 @@ namespace mRemoteNG {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("de,el,en,en-US,es-AR,es,fr,hu,it,ja-JP,nb-NO,nl,pt,pt-BR,pl,ru,uk,zh-CN,zh-TW")]
+        [global::System.Configuration.DefaultSettingValueAttribute("de,el,en,en-US,es-AR,es,fr,hu,it,ja-JP,nb-NO,nl,pt,pt-BR,pl,ru,uk,tr-TR,zh-CN,zh-" +
+            "TW")]
         public string SupportedUICultures {
             get {
                 return ((string)(this["SupportedUICultures"]));
