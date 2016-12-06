6367b589941c1d938f922e39f159a98a9c954f86
David Sparer <David.Sparer@pearson.com>
2016-12-06 15:28:56 -0700

    began adding unit tests for the connectiontree ui

---
+++ mRemoteNGTests/UI/Window/ConnectionTreeWindowTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading;
+using mRemoteNG.Container;
+using mRemoteNG.Tree;
+using mRemoteNG.Tree.Root;
+using mRemoteNG.UI.Window;
+using NUnit.Framework;
+using WeifenLuo.WinFormsUI.Docking;
+
+
+namespace mRemoteNGTests.UI.Window
+{
+    public class ConnectionTreeWindowTests
+    {
+        private ConnectionTreeWindow _connectionTreeWindow;
+
+        [SetUp]
+        public void Setup()
+        {
+            _connectionTreeWindow = new ConnectionTreeWindow(new DockContent());
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _connectionTreeWindow.Close();
+        }
+
+        [Test, Apartment(ApartmentState.STA)]
+        public void CanSetTheConnectionTreeModel()
+        {
+            var treeModel = CreateConnectionTreeModel();
+            _connectionTreeWindow.ConnectionTreeModel = treeModel;
+            _connectionTreeWindow.Show();
+            Assert.That(_connectionTreeWindow.ConnectionTreeModel, Is.EqualTo(treeModel));
+        }
+
+        [Test, Apartment(ApartmentState.STA)]
+        public void CanDeleteLastFolderInTheTree()
+        {
+            var treeModel = CreateConnectionTreeModel();
+            treeModel.RootNodes[0].AddChild(new ContainerInfo());
+            _connectionTreeWindow.ConnectionTreeModel = treeModel;
+            _connectionTreeWindow.Show();
+            Assert.That(_connectionTreeWindow.ConnectionTreeModel, Is.EqualTo(treeModel));
+        }
+
+        private ConnectionTreeModel CreateConnectionTreeModel()
+        {
+            var connectionTreeModel = new ConnectionTreeModel();
+            connectionTreeModel.AddRootNode(new RootNodeInfo(RootNodeType.Connection));
+            return connectionTreeModel;
+        }
+    }
+}
