051dd2e5674dd2c4e03533ea9b0c22d1bc137947
Sean Kaim <sean.kaim@gmail.com>
2017-01-04 11:15:51 -0500

    Add Beta and Dev tests

--- mRemoteNGTests/App/UpdaterTests.cs
+++ mRemoteNGTests/App/UpdaterTests.cs
@@ -8,20 +8,44 @@ namespace mRemoteNGTests.App
     [TestFixture]
     public class UpdaterTests
     {
-        /*
-        [SetUp]
-        public void Setup()
-        {
-            GeneralAppInfo.ApplicationVersion = "1.0.0.0";
-        }
-        */
-
         [Test]
         public void UpdateStableChannel()
         {
             GeneralAppInfo.ApplicationVersion = "1.0.0.0";
             var _upd = new AppUpdater();
             var e = _upd.DownloadString(UpdateChannelInfo.GetUpdateChannelInfo(UpdateChannelInfo.STABLE));
+            Assert.That(e.Cancelled, Is.False);
+            Assert.That(e.Error, Is.Null);
+            var CurrentUpdateInfo = UpdateInfo.FromString(e.Result);
+            Assert.That(CurrentUpdateInfo.CheckIfValid(), Is.True);
+            Version v;
+            Version.TryParse(GeneralAppInfo.ApplicationVersion, out v);
+            var IsNewer = CurrentUpdateInfo.Version > v;
+            Assert.That(IsNewer, Is.True);
+        }
+
+        [Test]
+        public void UpdateBetaChannel()
+        {
+            GeneralAppInfo.ApplicationVersion = "1.0.0.0";
+            var _upd = new AppUpdater();
+            var e = _upd.DownloadString(UpdateChannelInfo.GetUpdateChannelInfo(UpdateChannelInfo.BETA));
+            Assert.That(e.Cancelled, Is.False);
+            Assert.That(e.Error, Is.Null);
+            var CurrentUpdateInfo = UpdateInfo.FromString(e.Result);
+            Assert.That(CurrentUpdateInfo.CheckIfValid(), Is.True);
+            Version v;
+            Version.TryParse(GeneralAppInfo.ApplicationVersion, out v);
+            var IsNewer = CurrentUpdateInfo.Version > v;
+            Assert.That(IsNewer, Is.True);
+        }
+
+        [Test]
+        public void UpdateDevChannel()
+        {
+            GeneralAppInfo.ApplicationVersion = "1.0.0.0";
+            var _upd = new AppUpdater();
+            var e = _upd.DownloadString(UpdateChannelInfo.GetUpdateChannelInfo(UpdateChannelInfo.DEV));
             Assert.That(e.Cancelled, Is.False);
             Assert.That(e.Error, Is.Null);
             var CurrentUpdateInfo = UpdateInfo.FromString(e.Result);
