77add0b39d25fd18e0384c99c2358607088dd682
David Sparer <David.Sparer@pearson.com>
2017-01-12 15:08:12 -0700

    added an interface for iconnectiontree. this kind of hacky and really just exists to make the tests work. this indicates we have some coupling on ConnectionTree that shouldn't be there..

---
+++ mRemoteV1/UI/Controls/IConnectionTree.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using mRemoteNG.Tree;
+using mRemoteNG.Tree.Root;
+
+
+namespace mRemoteNG.UI.Controls
+{
+    public interface IConnectionTree
+    {
+        ConnectionTreeModel ConnectionTreeModel { get; set; }
+
+        IEnumerable ExpandedObjects { get; set; }
+
+        RootNodeInfo GetRootConnectionNode();
+
+        void InvokeExpand(object model);
+
+        void InvokeRebuildAll(bool preserveState);
+    }
+}
