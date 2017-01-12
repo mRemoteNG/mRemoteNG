6cc668fe83846f6078ed32803c88aa0095f859ff
David Sparer <David.Sparer@pearson.com>
2017-01-12 15:30:30 -0700

    added more tests for connectiontree delegates

---
+++ mRemoteNGTests/Tree/ExpandNodeClickHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using mRemoteNG.Connection;
+using mRemoteNG.Container;
+using mRemoteNG.Tree;
+using mRemoteNG.Tree.Root;
+using mRemoteNG.UI.Controls;
+using NSubstitute;
+using NUnit.Framework;
+
+
+namespace mRemoteNGTests.Tree
+{
+    public class ExpandNodeClickHandlerTests
+    {
+        private ExpandNodeClickHandler _clickHandler;
+        private IConnectionTree _connectionTree;
+
+        [SetUp]
+        public void Setup()
+        {
+            _connectionTree = Substitute.For<IConnectionTree>();
+            _clickHandler = new ExpandNodeClickHandler(_connectionTree);
+        }
+
+        [Test]
+        public void TargetedNodeIsExpanded()
+        {
+            var folder = new ContainerInfo();
+            _clickHandler.Execute(folder);
+            _connectionTree.Received().ToggleExpansion(folder);
+        }
+
+        [Test]
+        public void NothingHappensWhenConnectionInfoProvided()
+        {
+            _clickHandler.Execute(new ConnectionInfo());
+            _connectionTree.DidNotReceiveWithAnyArgs().ToggleExpansion(new ConnectionInfo());
+        }
+    }
+}
