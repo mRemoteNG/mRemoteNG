3998be3f9321f736af842ea1a0260dbbca015fa2
David Sparer <David.Sparer@pearson.com>
2017-01-20 10:10:59 -0700

    created credential saver and loader classes

---
+++ mRemoteV1/Config/CredentialRecordLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using mRemoteNG.Config.DataProviders;
+using mRemoteNG.Config.Serializers;
+using mRemoteNG.Credential;
+
+
+namespace mRemoteNG.Config
+{
+    public class CredentialRecordLoader
+    {
+        private readonly IDataProvider<string> _dataProvider;
+        private readonly XmlCredentialDeserializer _deserializer;
+
+        public CredentialRecordLoader(IDataProvider<string> dataProvider, XmlCredentialDeserializer deserializer)
+        {
+            if (dataProvider == null)
+                throw new ArgumentNullException(nameof(dataProvider));
+            if (deserializer == null)
+                throw new ArgumentNullException(nameof(deserializer));
+
+            _dataProvider = dataProvider;
+            _deserializer = deserializer;
+        }
+
+        public IEnumerable<ICredentialRecord> Load(SecureString decryptionKey)
+        {
+            var serializedCredentials = _dataProvider.Load();
+            return _deserializer.Deserialize(serializedCredentials, decryptionKey);
+        }
+    }
+}
