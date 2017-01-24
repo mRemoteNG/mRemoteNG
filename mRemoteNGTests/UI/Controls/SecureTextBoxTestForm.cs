61840874567545d72321041dbea066485d375214
David Sparer <David.Sparer@pearson.com>
2017-01-24 09:09:25 -0700

    added secure text box

---
+++ mRemoteNGTests/UI/Controls/SecureTextBoxTestForm.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace mRemoteNGTests.UI.Controls
+{
+    public partial class SecureTextBoxTestForm : Form
+    {
+        public SecureTextBoxTestForm()
+        {
+            Application.EnableVisualStyles();
+            InitializeComponent();
+        }
+    }
+}
