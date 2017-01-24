b55e42ba783d0d215b5c4b69189d57e4b1080021
David Sparer <David.Sparer@pearson.com>
2017-01-24 09:34:38 -0700

    added cue banner support for text boxes

---
+++ mRemoteNGTests/UI/Controls/TextBoxExtensionsTestForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace mRemoteNGTests.UI.Controls
+{
+    partial class TextBoxExtensionsTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // TextBoxExtensionsTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 48);
+            this.Controls.Add(this.textBox1);
+            this.Name = "TextBoxExtensionsTestForm";
+            this.Text = "TextBoxExtensionsTestForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ mRemoteNGTests/UI/Controls/TextBoxExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using mRemoteNG.UI.Controls;
+using NUnit.Extensions.Forms;
+using NUnit.Framework;
+
+namespace mRemoteNGTests.UI.Controls
+{
+    [TestFixture]
+    public class TextBoxExtensionsTests
+    {
+        private TextBoxExtensionsTestForm _textBoxExtensionsTestForm;
+
+        [SetUp]
+        public void Setup()
+        {
+            _textBoxExtensionsTestForm = new TextBoxExtensionsTestForm();
+            _textBoxExtensionsTestForm.Show();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _textBoxExtensionsTestForm.Dispose();
+            while (_textBoxExtensionsTestForm.Disposing)
+            { }
+            _textBoxExtensionsTestForm = null;
+        }
+
+        [Test]
+        public void SetCueBannerSetsTheBannerText()
+        {
+            const string text = "Type Here";
+            var textBox = new TextBoxTester(_textBoxExtensionsTestForm.textBox1.Name);
+            Assert.That(textBox.Properties.SetCueBannerText(text), Is.True);
+        }
+
+        [Test]
+        public void GetCueBannerReturnsCorrectValue()
+        {
+            const string text = "Type Here";
+            var textBox = new TextBoxTester(_textBoxExtensionsTestForm.textBox1.Name);
+            textBox.Properties.SetCueBannerText(text);
+            Assert.That(textBox.Properties.GetCueBannerText(), Is.EqualTo(text));
+        }
+    }
+}
