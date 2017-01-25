ca4d22602fcdcfaf6fa67e614a6fe40074bb3312
David Sparer <David.Sparer@pearson.com>
2017-01-25 08:16:29 -0700

    began creating credential harvester to help split credentials from confCons v2.6 to confCreds v1.0

---
+++ mRemoteNGTests/Config/CredentialHarvesterTests.cs
@@ -0,0 +1,86 @@
+﻿using System.Linq;
+using System.Security;
+using System.Xml.Linq;
+using mRemoteNG.Config;
+using mRemoteNG.Config.Serializers;
+using mRemoteNG.Connection;
+using mRemoteNG.Container;
+using mRemoteNG.Security;
+using mRemoteNG.Tree.Root;
+using NUnit.Framework;
+
+
+namespace mRemoteNGTests.Config
+{
+    public class CredentialHarvesterTests
+    {
+        private CredentialHarvester _credentialHarvester;
+        private ICryptographyProvider _cryptographyProvider;
+        private SecureString _key;
+
+        [SetUp]
+        public void Setup()
+        {
+            _credentialHarvester = new CredentialHarvester();
+            _cryptographyProvider = new CryptographyProviderFactory().CreateAeadCryptographyProvider(BlockCipherEngines.AES, BlockCipherModes.GCM);
+            _key = "testKey123".ConvertToSecureString();
+        }
+
+        [Test]
+        public void HarvestsUsername()
+        {
+            var connection = new ConnectionInfo { Username = "myuser", Domain = "somedomain", Password = "mypass" };
+            var xdoc = CreateTestData(connection);
+            var credentials = _credentialHarvester.Harvest(xdoc, _key);
+            Assert.That(credentials.Single().Username, Is.EqualTo(connection.Username));
+        }
+
+        [Test]
+        public void HarvestsDomain()
+        {
+            var connection = new ConnectionInfo { Username = "myuser", Domain = "somedomain", Password = "mypass" };
+            var xdoc = CreateTestData(connection);
+            var credentials = _credentialHarvester.Harvest(xdoc, _key);
+            Assert.That(credentials.Single().Domain, Is.EqualTo(connection.Domain));
+        }
+
+        [Test]
+        public void HarvestsPassword()
+        {
+            var connection = new ConnectionInfo { Username = "myuser", Domain = "somedomain", Password = "mypass" };
+            var xdoc = CreateTestData(connection);
+            var credentials = _credentialHarvester.Harvest(xdoc, _key);
+            Assert.That(credentials.Single().Password.ConvertToUnsecureString(), Is.EqualTo(connection.Password));
+        }
+
+        [Test]
+        public void DoesNotHarvestEmptyCredentials()
+        {
+            var connection = new ConnectionInfo();
+            var xdoc = CreateTestData(connection);
+            var credentials = _credentialHarvester.Harvest(xdoc, _key);
+            Assert.That(credentials.Count(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void HarvestsAllCredentials()
+        {
+            var container = new ContainerInfo();
+            var con1 = new ConnectionInfo {Username = "blah"};
+            var con2 = new ConnectionInfo {Username = "something"};
+            container.AddChildRange(new [] {con1, con2});
+            var xdoc = CreateTestData(container);
+            var credentials = _credentialHarvester.Harvest(xdoc, _key);
+            Assert.That(credentials.Count(), Is.EqualTo(2));
+        }
+
+        private XDocument CreateTestData(ConnectionInfo connectionInfo)
+        {
+            var rootNode = new RootNodeInfo(RootNodeType.Connection) {PasswordString = _key.ConvertToUnsecureString()};
+            rootNode.AddChild(connectionInfo);
+            var serialier = new XmlConnectionsSerializer(_cryptographyProvider);
+            var serializedData = serialier.Serialize(rootNode);
+            return XDocument.Parse(serializedData);
+        }
+    }
+}
---
+++ mRemoteV1/Config/CredentialHarvester.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Xml.Linq;
+using mRemoteNG.Credential;
+using mRemoteNG.Security;
+
+namespace mRemoteNG.Config
+{
+    public class CredentialHarvester
+    {
+        public IEnumerable<ICredentialRecord> Harvest(XDocument xDocument, SecureString decryptionKey)
+        {
+            if (xDocument == null)
+                throw new ArgumentNullException(nameof(xDocument));
+
+            var cryptoProvider = CryptographyProviderFactory.BuildFromXml(xDocument.Root);
+
+            var credentialList = from e in xDocument.Descendants("Node")
+                                 where EntryHasSomeCredentialData(e)
+                                 select new CredentialRecord
+                                {
+                                    Username = e.Attribute("Username")?.Value,
+                                    Domain =  e.Attribute("Domain")?.Value,
+                                    Password = cryptoProvider.Decrypt(e.Attribute("Password")?.Value, decryptionKey).ConvertToSecureString()
+                                };
+
+            return credentialList;
+        }
+
+        private static bool EntryHasSomeCredentialData(XElement e)
+        {
+            return e.Attribute("Username")?.Value != "" || 
+                   e.Attribute("Domain")?.Value != "" ||
+                   e.Attribute("Password")?.Value != "";
+        }
+    }
+}
