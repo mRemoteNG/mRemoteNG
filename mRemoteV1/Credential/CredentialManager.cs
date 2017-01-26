b953240966e75c12f8531d110696eca7ff01d735
David Sparer <David.Sparer@pearson.com>
2017-01-26 08:18:00 -0700

    created some ui elements to make selection credentials from the config window easier

---
+++ mRemoteV1/Credential/CredentialManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace mRemoteNG.Credential
+{
+    public class CredentialManager
+    {
+        private readonly IList<ICredentialRecord> _credentialRecords;
+
+        public CredentialManager()
+        {
+            _credentialRecords = new List<ICredentialRecord>();
+        }
+
+        public void Add(ICredentialRecord credentialRecord)
+        {
+            _credentialRecords.Add(credentialRecord);
+            RaiseCredentialsChangedEvent(this);
+        }
+
+        public void AddRange(IEnumerable<ICredentialRecord> credentialRecords)
+        {
+            foreach (var credential in credentialRecords)
+                _credentialRecords.Add(credential);
+            RaiseCredentialsChangedEvent(this);
+        }
+
+        public void Remove(ICredentialRecord credentialRecord)
+        {
+            _credentialRecords.Remove(credentialRecord);
+            RaiseCredentialsChangedEvent(this);
+        }
+
+        public IEnumerable<ICredentialRecord> GetCredentialRecords()
+        {
+            return _credentialRecords;
+        }
+
+        public event EventHandler CredentialsChanged;
+        private void RaiseCredentialsChangedEvent(object sender)
+        {
+            CredentialsChanged?.Invoke(sender, EventArgs.Empty);
+        }
+    }
+}
---
+++ mRemoteV1/UI/Controls/CredentialRecordTypeEditorAdapter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Windows.Forms.Design;
+using mRemoteNG.App;
+using mRemoteNG.UI.Forms;
+
+
+namespace mRemoteNG.UI.Controls
+{
+    public class CredentialRecordTypeEditorAdapter : UITypeEditor
+    {
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.Modal;
+        }
+
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            var svc = provider.GetService(typeof(IWindowsFormsEditorService)) as IWindowsFormsEditorService;
+
+            using (var form = new CredentialManagerForm(Runtime.CredentialManager))
+            {
+                svc.ShowDialog(form);
+            }
+            return value; // can also replace the wrapper object here
+        }
+    }
+}
