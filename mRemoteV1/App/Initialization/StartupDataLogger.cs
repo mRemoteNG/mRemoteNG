9ddc45721f5175ec8de60e9ef68043d574ea1b94
David Sparer <David.Sparer@pearson.com>
2017-01-30 12:12:16 -0700

    extracted some classes from Startup

---
+++ mRemoteV1/App/Initialization/StartupDataLogger.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Management;
+using System.Threading;
+using System.Windows.Forms;
+using log4net;
+
+
+namespace mRemoteNG.App.Initialization
+{
+    public class StartupDataLogger
+    {
+        private readonly ILog _logger;
+
+        public StartupDataLogger(ILog logger)
+        {
+            if (logger == null)
+                throw new ArgumentNullException(nameof(logger));
+
+            _logger = logger;
+        }
+
+        public void Execute()
+        {
+            if (!Settings.Default.WriteLogFile) return;
+            LogApplicationData();
+            LogCmdLineArgs();
+            LogSystemData();
+            LogClrData();
+            LogCultureData();
+        }
+
+        private void LogSystemData()
+        {
+            var osData = GetOperatingSystemData();
+            var architecture = GetArchitectureData();
+            _logger.InfoFormat(string.Join(" ", Array.FindAll(new[] { osData, architecture }, s => !string.IsNullOrEmpty(Convert.ToString(s)))));
+        }
+
+        private string GetOperatingSystemData()
+        {
+            var osVersion = string.Empty;
+            var servicePack = string.Empty;
+
+            try
+            {
+                foreach (var o in new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem WHERE Primary=True").Get())
+                {
+                    var managementObject = (ManagementObject)o;
+                    osVersion = Convert.ToString(managementObject.GetPropertyValue("Caption")).Trim();
+                    servicePack = GetOSServicePack(servicePack, managementObject);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.WarnFormat($"Error retrieving operating system information from WMI. {ex.Message}");
+            }
+            var osData = string.Join(" ", osVersion, servicePack);
+            return osData;
+        }
+
+        private string GetOSServicePack(string servicePack, ManagementObject managementObject)
+        {
+            var servicePackNumber = Convert.ToInt32(managementObject.GetPropertyValue("ServicePackMajorVersion"));
+            if (servicePackNumber != 0)
+            {
+                servicePack = $"Service Pack {servicePackNumber}";
+            }
+            return servicePack;
+        }
+
+        private string GetArchitectureData()
+        {
+            var architecture = string.Empty;
+            try
+            {
+                foreach (var o in new ManagementObjectSearcher("SELECT * FROM Win32_Processor WHERE DeviceID=\'CPU0\'").Get())
+                {
+                    var managementObject = (ManagementObject)o;
+                    var addressWidth = Convert.ToInt32(managementObject.GetPropertyValue("AddressWidth"));
+                    architecture = $"{addressWidth}-bit";
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.WarnFormat($"Error retrieving operating system address width from WMI. {ex.Message}");
+            }
+            return architecture;
+        }
+
+        private void LogApplicationData()
+        {
+#if !PORTABLE
+            _logger.InfoFormat($"{Application.ProductName} {Application.ProductVersion} starting.");
+#else
+            _logger.InfoFormat($"{Application.ProductName} {Application.ProductVersion} {Language.strLabelPortableEdition} starting.");
+#endif
+        }
+
+        private void LogCmdLineArgs()
+        {
+            _logger.InfoFormat($"Command Line: {Environment.GetCommandLineArgs()}");
+        }
+
+        private void LogClrData()
+        {
+            _logger.InfoFormat($"Microsoft .NET CLR {Environment.Version}");
+        }
+
+        private void LogCultureData()
+        {
+            _logger.InfoFormat($"System Culture: {Thread.CurrentThread.CurrentUICulture.Name}/{Thread.CurrentThread.CurrentUICulture.NativeName}");
+        }
+    }
+}
--- mRemoteV1/App/Startup.cs
+++ mRemoteV1/App/Startup.cs
@@ -4,10 +4,9 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
 using System.IO;
-using System.Management;
-using System.Threading;
 using System.Windows.Forms;
 using mRemoteNG.App.Info;
+using mRemoteNG.App.Initialization;
 using mRemoteNG.App.Update;
 using mRemoteNG.Config.Connections;
 using mRemoteNG.Config.Connections.Multiuser;
@@ -17,17 +16,22 @@ using mRemoteNG.Tools;
 using mRemoteNG.UI;
 using mRemoteNG.UI.Forms;
 
+
 namespace mRemoteNG.App
 {
     public class Startup
     {
         private AppUpdater _appUpdate;
+        private readonly StartupDataLogger _startupDataLogger;
+        private readonly ConnectionIconLoader _connectionIconLoader;
 
         public static Startup Instance { get; } = new Startup();
 
         private Startup()
         {
             _appUpdate = new AppUpdater();
+            _startupDataLogger = new StartupDataLogger(Logger.Instance);
+            _connectionIconLoader = new ConnectionIconLoader(GeneralAppInfo.HomePath + "\\Icons\\");
         }
 
         static Startup()
@@ -37,123 +41,13 @@ namespace mRemoteNG.App
         public void InitializeProgram()
         {
             Debug.Print("---------------------------" + Environment.NewLine + "[START] - " + Convert.ToString(DateTime.Now, CultureInfo.InvariantCulture));
-            LogStartupData();
+            _startupDataLogger.Execute();
             CompatibilityChecker.CheckCompatibility();
             ParseCommandLineArgs();
             IeBrowserEmulation.Register();
-            GetConnectionIcons();
+            _connectionIconLoader.GetConnectionIcons();
             DefaultConnectionInfo.Instance.LoadFrom(Settings.Default, a=>"ConDefault"+a);
             DefaultConnectionInheritance.Instance.LoadFrom(Settings.Default, a=>"InhDefault"+a);
-        }
-
-        private static void GetConnectionIcons()
-        {
-            var iPath = GeneralAppInfo.HomePath + "\\Icons\\";
-            if (Directory.Exists(iPath) == false)
-            {
-                return;
-            }
-
-            foreach (var f in Directory.GetFiles(iPath, "*.ico", SearchOption.AllDirectories))
-            {
-                var fInfo = new FileInfo(f);
-                Array.Resize(ref ConnectionIcon.Icons, ConnectionIcon.Icons.Length + 1);
-                ConnectionIcon.Icons.SetValue(fInfo.Name.Replace(".ico", ""), ConnectionIcon.Icons.Length - 1);
-            }
-        }
-
-        private static void LogStartupData()
-        {
-            if (!Settings.Default.WriteLogFile) return;
-            LogApplicationData();
-            LogCmdLineArgs();
-            LogSystemData();
-            LogCLRData();
-            LogCultureData();
-        }
-
-        private static void LogSystemData()
-        {
-            var osData = GetOperatingSystemData();
-            var architecture = GetArchitectureData();
-            Logger.Instance.InfoFormat(string.Join(" ", Array.FindAll(new[] { osData, architecture }, s => !string.IsNullOrEmpty(Convert.ToString(s)))));
-        }
-
-        private static string GetOperatingSystemData()
-        {
-            var osVersion = string.Empty;
-            var servicePack = string.Empty;
-
-            try
-            {
-                foreach (var o in new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem WHERE Primary=True").Get())
-                {
-                    var managementObject = (ManagementObject) o;
-                    osVersion = Convert.ToString(managementObject.GetPropertyValue("Caption")).Trim();
-                    servicePack = GetOSServicePack(servicePack, managementObject);
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Instance.WarnFormat($"Error retrieving operating system information from WMI. {ex.Message}");
-            }
-            var osData = string.Join(" ", osVersion, servicePack);
-            return osData;
-        }
-
-        private static string GetOSServicePack(string servicePack, ManagementObject managementObject)
-        {
-            var servicePackNumber = Convert.ToInt32(managementObject.GetPropertyValue("ServicePackMajorVersion"));
-            if (servicePackNumber != 0)
-            {
-                servicePack = $"Service Pack {servicePackNumber}";
-            }
-            return servicePack;
-        }
-
-        private static string GetArchitectureData()
-        {
-            var architecture = string.Empty;
-            try
-            {
-                foreach (var o in new ManagementObjectSearcher("SELECT * FROM Win32_Processor WHERE DeviceID=\'CPU0\'").Get())
-                {
-                    var managementObject = (ManagementObject) o;
-                    var addressWidth = Convert.ToInt32(managementObject.GetPropertyValue("AddressWidth"));
-                    architecture = $"{addressWidth}-bit";
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.Instance.WarnFormat($"Error retrieving operating system address width from WMI. {ex.Message}");
-            }
-            return architecture;
-        }
-
-        private static void LogApplicationData()
-        {
-#if !PORTABLE
-            Logger.Instance.InfoFormat($"{Application.ProductName} {Application.ProductVersion} starting.");
-#else
-            Logger.Instance.InfoFormat(
-                $"{Application.ProductName} {Application.ProductVersion} {Language.strLabelPortableEdition} starting.");
-#endif
-        }
-
-        private static void LogCmdLineArgs()
-        {
-            Logger.Instance.InfoFormat($"Command Line: {Environment.GetCommandLineArgs()}");
-        }
-
-        private static void LogCLRData()
-        {
-            Logger.Instance.InfoFormat($"Microsoft .NET CLR {Environment.Version}");
-        }
-
-        private static void LogCultureData()
-        {
-            Logger.Instance.InfoFormat(
-                $"System Culture: {Thread.CurrentThread.CurrentUICulture.Name}/{Thread.CurrentThread.CurrentUICulture.NativeName}");
         }
 
         public void CreateConnectionsProvider()
@@ -217,7 +111,6 @@ namespace mRemoteNG.App
                 Runtime.MessageCollector.AddExceptionMessage("GetUpdateInfoCompleted() failed.", ex, MessageClass.ErrorMsg, true);
             }
         }
-
 
         private static void ParseCommandLineArgs()
         {
