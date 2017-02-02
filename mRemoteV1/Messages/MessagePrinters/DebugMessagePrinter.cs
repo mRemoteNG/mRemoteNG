6beb21349bc584710dd2d96c258b4412550cc8b9
David Sparer <sparerd@gmail.com>
2017-02-02 12:07:51 -0700

    created a message printer for writing to the debug stream

---
+++ mRemoteV1/Messages/MessagePrinters/DebugMessagePrinter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace mRemoteNG.Messages.MessagePrinters
+{
+    public class DebugMessagePrinter : IMessagePrinter
+    {
+        public void Print(IMessage message)
+        {
+            var textToPrint = $"{message.Class}: {message.Text}";
+            Debug.Print(textToPrint);
+        }
+
+        public void Print(IEnumerable<IMessage> messages)
+        {
+            foreach (var message in messages)
+                Print(message);
+        }
+    }
+}
