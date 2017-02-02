b2240e8f1ab30dc4282147506f470c0560e51760
David Sparer <sparerd@gmail.com>
2017-02-02 12:07:08 -0700

    created a replacement message collector that has no dependency on whatever printers may want to show its messages

---
+++ mRemoteNGTests/Messages/MessageCollectorTests.cs
@@ -0,0 +1,60 @@
+﻿using mRemoteNG.Messages;
+using NSubstitute;
+using NUnit.Framework;
+
+
+namespace mRemoteNGTests.Messages
+{
+    public class MessageCollectorTests
+    {
+        private MessageCollector2 _messageCollector;
+
+        [SetUp]
+        public void Setup()
+        {
+            _messageCollector = new MessageCollector2();
+        }
+
+        [Test]
+        public void InitializesWithNoMessages()
+        {
+            Assert.That(_messageCollector.Messages, Is.Empty);
+        }
+
+        [Test]
+        public void MessageAddedToList()
+        {
+            var message = Substitute.For<IMessage>();
+            _messageCollector.AddMessage(message);
+            Assert.That(_messageCollector.Messages, Does.Contain(message));
+        }
+
+        [Test]
+        public void ListCanBeCleared()
+        {
+            var message = Substitute.For<IMessage>();
+            _messageCollector.AddMessage(message);
+            _messageCollector.ClearMessages();
+            Assert.That(_messageCollector.Messages, Is.Empty);
+        }
+
+        [Test]
+        public void MessagesAreUnique()
+        {
+            var message = Substitute.For<IMessage>();
+            _messageCollector.AddMessage(message);
+            _messageCollector.AddMessage(message);
+            Assert.That(_messageCollector.Messages, Is.Unique);
+        }
+
+        [Test]
+        public void NotifiedWhenMessageAdded()
+        {
+            var wasCalled = false;
+            _messageCollector.CollectionChanged += (sender, args) => wasCalled = true;
+            var message = Substitute.For<IMessage>();
+            _messageCollector.AddMessage(message);
+            Assert.That(wasCalled, Is.True);
+        }
+    }
+}
---
+++ mRemoteV1/Messages/MessageCollector2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+
+namespace mRemoteNG.Messages
+{
+    public class MessageCollector2 : INotifyCollectionChanged
+    {
+        private readonly IList<IMessage> _messageList;
+
+        public IEnumerable<IMessage> Messages => _messageList;
+
+        public MessageCollector2()
+        {
+            _messageList = new ObservableCollection<IMessage>();
+            ((INotifyCollectionChanged) _messageList).CollectionChanged += RaiseCollectionChangedEvent;
+        }
+
+        public void AddMessage(MessageClass messageClass, string messageText)
+        {
+            var message = new Message(messageClass, messageText, DateTime.Now);
+            AddMessage(message);
+        }
+
+        public void AddMessage(IMessage message)
+        {
+            if (_messageList.Contains(message)) return;
+            _messageList.Add(message);
+        }
+
+        public void ClearMessages()
+        {
+            _messageList.Clear();
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        private void RaiseCollectionChangedEvent(object sender, NotifyCollectionChangedEventArgs args)
+        {
+            CollectionChanged?.Invoke(this, args);
+        }
+    }
+}
