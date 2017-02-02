32f43d46b501be6be539785bc67d672a14a93cf2
David Sparer <sparerd@gmail.com>
2017-02-02 12:05:32 -0700

    created an interface for message printers

---
+++ mRemoteV1/Messages/MessagePrinters/IMessagePrinter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace mRemoteNG.Messages.MessagePrinters
+{
+    public interface IMessagePrinter
+    {
+        void Print(IMessage message);
+
+        void Print(IEnumerable<IMessage> messages);
+    }
+}
