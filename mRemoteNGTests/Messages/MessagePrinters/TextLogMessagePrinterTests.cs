bdfc8390801ca4a2c5017c33cea7b65d2cf48f5e
David Sparer <sparerd@gmail.com>
2017-02-02 12:08:39 -0700

    created a message printer adapter for the log4net logger

---
+++ mRemoteNGTests/Messages/MessagePrinters/TextLogMessagePrinterTests.cs
@@ -0,0 +1,108 @@
+﻿using log4net;
+using mRemoteNG.Messages;
+using mRemoteNG.Messages.MessagePrinters;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace mRemoteNGTests.Messages.MessagePrinters
+{
+    public class TextLogMessagePrinterTests
+    {
+        private TextLogMessagePrinter _messagePrinter;
+        private ILog _log4NetLogger;
+
+        [SetUp]
+        public void Setup()
+        {
+            _log4NetLogger = Substitute.For<ILog>();
+            _messagePrinter = new TextLogMessagePrinter(_log4NetLogger);
+        }
+
+        [Test]
+        public void MultipleMessagesPassedToLog4Net()
+        {
+            var message1 = Substitute.For<IMessage>();
+            var message2 = Substitute.For<IMessage>();
+            message1.Class.Returns(MessageClass.InformationMsg);
+            message2.Class.Returns(MessageClass.InformationMsg);
+            _messagePrinter.Print(new[] {message1, message2});
+            _log4NetLogger.ReceivedWithAnyArgs(2).Info("");
+        }
+
+        [Test]
+        public void InfoMessagesPassedToLog4Net()
+        {
+            var message = Substitute.For<IMessage>();
+            message.Class.Returns(MessageClass.InformationMsg);
+            _messagePrinter.Print(message);
+            _log4NetLogger.ReceivedWithAnyArgs(1).Info("");
+        }
+
+        [Test]
+        public void ReportMessagesPassedToLog4Net()
+        {
+            var message = Substitute.For<IMessage>();
+            message.Class.Returns(MessageClass.ReportMsg);
+            _messagePrinter.Print(message);
+            _log4NetLogger.ReceivedWithAnyArgs(1).Info("");
+        }
+
+        [Test]
+        public void WarningMessagesPassedToLog4Net()
+        {
+            var message = Substitute.For<IMessage>();
+            message.Class.Returns(MessageClass.WarningMsg);
+            _messagePrinter.Print(message);
+            _log4NetLogger.ReceivedWithAnyArgs(1).Warn("");
+        }
+
+        [Test]
+        public void ErrorMessagesPassedToLog4Net()
+        {
+            var message = Substitute.For<IMessage>();
+            message.Class.Returns(MessageClass.ErrorMsg);
+            _messagePrinter.Print(message);
+            _log4NetLogger.ReceivedWithAnyArgs(1).Error("");
+        }
+
+        [Test]
+        public void InfoMessagesNotPassedToLog4NetIfTurnedOff()
+        {
+            var message = Substitute.For<IMessage>();
+            message.Class.Returns(MessageClass.InformationMsg);
+            _messagePrinter.PrintInfoMessages = false;
+            _messagePrinter.Print(message);
+            _log4NetLogger.DidNotReceiveWithAnyArgs().Info("");
+        }
+
+        [Test]
+        public void ReportMessagesNotPassedToLog4NetIfTurnedOff()
+        {
+            var message = Substitute.For<IMessage>();
+            message.Class.Returns(MessageClass.ReportMsg);
+            _messagePrinter.PrintReportMessages = false;
+            _messagePrinter.Print(message);
+            _log4NetLogger.DidNotReceiveWithAnyArgs().Info("");
+        }
+
+        [Test]
+        public void WarningMessagesNotPassedToLog4NetIfTurnedOff()
+        {
+            var message = Substitute.For<IMessage>();
+            message.Class.Returns(MessageClass.WarningMsg);
+            _messagePrinter.PrintWarningMessages = false;
+            _messagePrinter.Print(message);
+            _log4NetLogger.DidNotReceiveWithAnyArgs().Warn("");
+        }
+
+        [Test]
+        public void ErrorMessagesNotPassedToLog4NetIfTurnedOff()
+        {
+            var message = Substitute.For<IMessage>();
+            message.Class.Returns(MessageClass.ErrorMsg);
+            _messagePrinter.PrintErrorMessages = false;
+            _messagePrinter.Print(message);
+            _log4NetLogger.DidNotReceiveWithAnyArgs().Error("");
+        }
+    }
+}
---
+++ mRemoteV1/Messages/MessagePrinters/TextLogMessagePrinter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using log4net;
+
+namespace mRemoteNG.Messages.MessagePrinters
+{
+    public class TextLogMessagePrinter : IMessagePrinter
+    {
+        private readonly ILog _logger;
+
+        public bool PrintReportMessages { get; set; } = true;
+        public bool PrintInfoMessages { get; set; } = true;
+        public bool PrintWarningMessages { get; set; } = true;
+        public bool PrintErrorMessages { get; set; } = true;
+
+        public TextLogMessagePrinter(ILog logger)
+        {
+            if (logger == null)
+                throw new ArgumentNullException(nameof(logger));
+
+            _logger = logger;
+        }
+
+        public void Print(IMessage message)
+        {
+            switch (message.Class)
+            {
+                case MessageClass.InformationMsg:
+                    if (PrintInfoMessages)
+                        _logger.Info(message.Text);
+                    break;
+                case MessageClass.ReportMsg:
+                    if (PrintReportMessages)
+                        _logger.Info(message.Text);
+                    break;
+                case MessageClass.WarningMsg:
+                    if (PrintWarningMessages)
+                        _logger.Warn(message.Text);
+                    break;
+                case MessageClass.ErrorMsg:
+                    if (PrintErrorMessages)
+                        _logger.Error(message.Text);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void Print(IEnumerable<IMessage> messages)
+        {
+            foreach (var message in messages)
+                Print(message);
+        }
+    }
+}
