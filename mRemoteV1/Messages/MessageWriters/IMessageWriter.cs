5840e2ba078fba818b48517c74c758242c68232d
David Sparer <David.Sparer@pearson.com>
2017-02-06 08:26:29 -0700

    moved all message filter options to a new interface

--- mRemoteV1/Messages/MessageWriters/IMessageWriter.cs
+++ mRemoteV1/Messages/MessageWriters/IMessageWriter.cs
@@ -2,11 +2,6 @@
 {
     public interface IMessageWriter
     {
-        bool AllowDebugMessages { get; set; }
-        bool AllowInfoMessages { get; set; }
-        bool AllowWarningMessages { get; set; }
-        bool AllowErrorMessages { get; set; }
-
         void Write(IMessage message);
     }
 }
