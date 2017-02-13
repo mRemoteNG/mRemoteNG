a3b66ec456c5286ddf3243179fdd64505d209bd7
David Sparer <David.Sparer@pearson.com>
2017-02-13 12:05:55 -0700

    defined new event args classes

---
+++ mRemoteV1/Credential/CredentialRepositoryChangedArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace mRemoteNG.Credential
+{
+    public class CredentialRepositoryChangedArgs
+    {
+        public ICredentialRepository Repository { get; }
+
+        public CredentialRepositoryChangedArgs(ICredentialRepository repository)
+        {
+            if (repository == null)
+                throw new ArgumentNullException(nameof(repository));
+
+            Repository = repository;
+        }
+    }
+}
