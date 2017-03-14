d2e33ee42335c4b1a1b6442d52e6a057dfc90d0b
Sean Kaim <sean.kaim@gmail.com>
2017-03-14 11:58:51 -0400

    I was doing some backwards thinking it appears...

--- mRemoteV1/Connection/Protocol/Http/Connection.Protocol.HTTPS.CertEvent.cs
+++ mRemoteV1/Connection/Protocol/Http/Connection.Protocol.HTTPS.CertEvent.cs
@@ -1,6 +1,8 @@
 ﻿using Gecko;
 using Gecko.Events;
+using mRemoteNG.App;
 using mRemoteNG.App.Info;
+using mRemoteNG.Messages;
 using mRemoteNG.UI.TaskDialog;
 // ReSharper disable RedundantAssignment
 
@@ -8,12 +10,16 @@ namespace mRemoteNG.Connection.Protocol.Http
 {
     internal abstract class CertEvent
     {
-
         //Refernce: https://bitbucket.org/geckofx/geckofx-33.0/issues/90/invalid-security-certificate-error-on
         internal static void GeckoBrowser_NSSError(object sender, GeckoNSSErrorEventArgs e)
         {
-            if (!e.Message.Contains("Certificate"))
+            /* some messages say "Certificate", some say "certificate"
+             * I'm guessing that this is going to be a localization issue... 
+             * Log a message so we can try to find a better solution if problems are reported in the future...
+             */
+            if (!e.Message.ToLower().Contains("certificate"))
             {
+                Runtime.MessageCollector.AddMessage(MessageClass.WarningMsg, $"Unhandled NSSError: {e.Message}");
                 e.Handled = false;
                 return;
             }
@@ -26,34 +32,38 @@ namespace mRemoteNG.Connection.Protocol.Http
             };
 
             CTaskDialog.ShowTaskDialogBox(null, GeneralAppInfo.ProductName, $"Allow Insecure Certificate?",
-                string.Format($"Allow Insecure Certificate for URL: {0}?", e.Uri.AbsoluteUri),
+                string.Format("Allow Insecure Certificate for URL: {0}?", e.Uri.AbsoluteUri),
                 "", "", "", "", string.Join(" | ", commandButtons), ETaskDialogButtons.None, ESysIcons.Question,
                 ESysIcons.Question);
 
            
             var allow = false;
-            var always = false;
+            var temporary = true;
             // ReSharper disable once SwitchStatementMissingSomeCases
             switch (CTaskDialog.CommandButtonResult)
             {
                 case 0:
                     allow = true;
-                    always = false;
+                    temporary = true;
                     break;
                 case 1:
                     allow = true;
-                    always = true;
+                    temporary = false;
                     break;
                 case 2:
                     allow = false;
-                    always = false;
+                    temporary = true; // just to be safe
                     break;
             }
 
-            // "always" might not work: https://bitbucket.org/geckofx/geckofx-45.0/issues/152/remembervalidityoverride-doesnt-save-in
-            if(allow)
-                CertOverrideService.GetService().RememberValidityOverride(e.Uri, e.Certificate, CertOverride.Mismatch | CertOverride.Time | CertOverride.Untrusted, always);
+            /* "temporary == false" (aka always) might not work: 
+             * https://bitbucket.org/geckofx/geckofx-45.0/issues/152/remembervalidityoverride-doesnt-save-in
+             * However, my testing was successful in Gecko 45.0.22
+             */
+            if (allow)
+                CertOverrideService.GetService().RememberValidityOverride(e.Uri, e.Certificate, CertOverride.Mismatch | CertOverride.Time | CertOverride.Untrusted, temporary);
             e.Handled = true;
+            // navigate even if we don't trust the cert. This will allow Gecko the return errors to the user.
             ((GeckoWebBrowser)sender).Navigate(e.Uri.AbsoluteUri);
         }
     }
