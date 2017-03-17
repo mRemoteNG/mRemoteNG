2293a14a90da6719b1a92943846814184aae1436
Sean Kaim <sean.kaim@gmail.com>
2017-03-17 17:51:04 -0400

    sysTray object safehandle implementation

--- mRemoteV1/Tools/Tools.SystemMenu.cs
+++ mRemoteV1/Tools/Tools.SystemMenu.cs
@@ -1,54 +1,84 @@
 using System;
 using System.Drawing;
-
+using mRemoteNG.App;
+using Microsoft.Win32.SafeHandles;
+// ReSharper disable MemberCanBeMadeStatic.Global
 
 namespace mRemoteNG.Tools
 {
-	public class SystemMenu
-	{
+	public sealed class SystemMenu : SafeHandleZeroOrMinusOneIsInvalid, IDisposable
+    {
 		[Flags]
 		public enum Flags
 		{
-			MF_STRING = App.NativeMethods.MF_STRING,
-			MF_SEPARATOR = App.NativeMethods.MF_SEPARATOR,
-			MF_BYCOMMAND = App.NativeMethods.MF_BYCOMMAND,
-			MF_BYPOSITION = App.NativeMethods.MF_BYPOSITION,
-			MF_POPUP = App.NativeMethods.MF_POPUP,
-			WM_SYSCOMMAND = App.NativeMethods.WM_SYSCOMMAND
+			MF_STRING = NativeMethods.MF_STRING,
+			MF_SEPARATOR = NativeMethods.MF_SEPARATOR,
+			MF_BYCOMMAND = NativeMethods.MF_BYCOMMAND,
+			MF_BYPOSITION = NativeMethods.MF_BYPOSITION,
+			MF_POPUP = NativeMethods.MF_POPUP,
+			WM_SYSCOMMAND = NativeMethods.WM_SYSCOMMAND
 		}
-			
-		public IntPtr SystemMenuHandle;
-		public IntPtr FormHandle;
-			
-		public SystemMenu(IntPtr Handle)
+
+        private bool disposed;
+        internal IntPtr SystemMenuHandle;
+        private readonly IntPtr FormHandle;
+
+        public SystemMenu(IntPtr Handle) :base(true)
 		{
 			FormHandle = Handle;
-			SystemMenuHandle = App.NativeMethods.GetSystemMenu(FormHandle, false);
-		}
+            SystemMenuHandle = NativeMethods.GetSystemMenu(FormHandle, false);
+            SetHandle(SystemMenuHandle);
+        }
 			
 		public void Reset()
 		{
-			SystemMenuHandle = App.NativeMethods.GetSystemMenu(FormHandle, true);
+			SystemMenuHandle = NativeMethods.GetSystemMenu(FormHandle, true);
 		}
 
         public void AppendMenuItem(IntPtr ParentMenu, Flags Flags, IntPtr ID, string Text)
 		{
-			App.NativeMethods.AppendMenu(ParentMenu, (int)Flags, ID, Text);
+			NativeMethods.AppendMenu(ParentMenu, (int)Flags, ID, Text);
 		}
 			
 		public IntPtr CreatePopupMenuItem()
 		{
-			return App.NativeMethods.CreatePopupMenu();
+			return NativeMethods.CreatePopupMenu();
 		}
 			
 		public bool InsertMenuItem(IntPtr SysMenu, int Position, Flags Flags, IntPtr SubMenu, string Text)
 		{
-			return App.NativeMethods.InsertMenu(SysMenu, Position, (int)Flags, SubMenu, Text);
+			return NativeMethods.InsertMenu(SysMenu, Position, (int)Flags, SubMenu, Text);
 		}
 			
 		public IntPtr SetBitmap(IntPtr Menu, int Position, Flags Flags, Bitmap Bitmap)
 		{
-			return new IntPtr(Convert.ToInt32(App.NativeMethods.SetMenuItemBitmaps(Menu, Position, (int)Flags, Bitmap.GetHbitmap(), Bitmap.GetHbitmap())));
+			return new IntPtr(Convert.ToInt32(NativeMethods.SetMenuItemBitmaps(Menu, Position, (int)Flags, Bitmap.GetHbitmap(), Bitmap.GetHbitmap())));
 		}
-	}
+
+        protected override bool ReleaseHandle()
+        {
+            return NativeMethods.CloseHandle(SystemMenuHandle);
+        }
+
+        ~SystemMenu()
+        {
+            Dispose(false);
+        }
+
+        public new void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposed) return;
+            if (!disposing) return;
+
+            ReleaseHandle();
+
+            disposed = true;
+        }
+    }
 }
