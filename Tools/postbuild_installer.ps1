84ebb82caef0837f9fb64dc9599de7a3a8d47aab
David Sparer <sparerd@gmail.com>
2017-03-18 00:24:39 -0600

    the installer project now uses a powershell script for post build actions

--- Tools/postbuild_installer.ps1
+++ Tools/postbuild_installer.ps1
@@ -16,23 +16,13 @@ param (
     $ConfigurationName
 )
 
-$certificatePath = "C:\mRemoteNG_code_signing_cert.pfx"
-$path_signFilesScript = Join-Path -Path $SolutionDir -ChildPath "Tools\signfiles.ps1"
-$path_installerRenameScript = Join-Path -Path $SolutionDir -ChildPath "Tools\rename_installer_with_version.ps1"
-$path_copyToReleaseScript = Join-Path -Path $SolutionDir -ChildPath "Tools\copy_release_installer.ps1"
+Write-Output "+=================================================================+"
+Write-Output "|             Beginning mRemoteNG Installer Post Build            |"
+Write-Output "+=================================================================+"
+Write-Output ([pscustomobject]$PSBoundParameters) | Format-Table -AutoSize -Wrap
 
 
-# Sign MSI if we are building a release version and the certificate is available
-if (($ConfigurationName -match "Release") -and (Test-Path -Path $certificatePath -PathType Leaf)) {
-    powershell.exe -ExecutionPolicy Bypass -File $path_signFilesScript $TargetDir
-}
-
-
-# Rename MSI to include version number
-powershell.exe -ExecutionPolicy Bypass -File $path_installerRenameScript -SolutionDir $SolutionDir
-
-
-# Copy MSI to Release folder
-if ($ConfigurationName -match "Release") {
-    powershell.exe -ExecutionPolicy Bypass -File $path_copyToReleaseScript -SourcePath $TargetDir -DestinationDir (Join-Path -Path $SolutionDir -ChildPath "Release")
-}
+& "$PSScriptRoot\sign_binaries.ps1" -SolutionDir $SolutionDir -TargetDir $TargetDir -ConfigurationName $ConfigurationName
+& "$PSScriptRoot\verify_binary_signatures.ps1" -TargetDir $TargetDir -ConfigurationName $ConfigurationName
+& "$PSScriptRoot\rename_installer_with_version.ps1" -SolutionDir $SolutionDir
+& "$PSScriptRoot\copy_release_installer.ps1" -SourcePath $TargetDir -DestinationDir (Join-Path -Path $SolutionDir -ChildPath "Release")
