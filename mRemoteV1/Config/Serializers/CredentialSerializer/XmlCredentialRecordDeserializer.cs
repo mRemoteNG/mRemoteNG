e886619b221fb8fc5ba6d11bee935a45f93ac8c5
David Sparer <sparerd@gmail.com>
2017-04-02 17:22:42 -0600

    simplified the implementation of the xml cred deserializer
    removed anything to do with encryption which will be handled by decorators

--- mRemoteV1/Config/Serializers/CredentialSerializer/XmlCredentialRecordDeserializer.cs
+++ mRemoteV1/Config/Serializers/CredentialSerializer/XmlCredentialRecordDeserializer.cs
@@ -5,32 +5,25 @@ using System.Security;
 using System.Xml.Linq;
 using mRemoteNG.Credential;
 using mRemoteNG.Security;
-using mRemoteNG.Security.Authentication;
 
-
-namespace mRemoteNG.Config.Serializers
+namespace mRemoteNG.Config.Serializers.CredentialSerializer
 {
     public class XmlCredentialRecordDeserializer
     {
         public string SchemaVersion { get; } = "1.0";
-        public IAuthenticator Authenticator { get; set; }
 
         public IEnumerable<ICredentialRecord> Deserialize(string xml, SecureString decryptionKey)
         {
             var xdoc = XDocument.Parse(xml);
             var rootElement = xdoc.Root;
             ValidateSchemaVersion(rootElement);
-            var cryptographyProvider = CryptographyProviderFactory.BuildFromXml(rootElement);
-            Authenticate(rootElement, cryptographyProvider, decryptionKey);
 
             var credentials = from element in xdoc.Descendants("Credential")
-                select new CredentialRecord(Guid.Parse(element.Attribute("Id")?.Value))
+                select new CredentialRecord(Guid.Parse(element.Attribute("Id")?.Value ?? Guid.NewGuid().ToString()))
                 {
                     Title = element.Attribute("Title")?.Value ?? "",
                     Username = element.Attribute("Username")?.Value ?? "",
-                    Password =
-                        cryptographyProvider.Decrypt(element.Attribute("Password")?.Value, decryptionKey)
-                            .ConvertToSecureString(),
+                    Password = element.Attribute("Password")?.Value.ConvertToSecureString(),
                     Domain = element.Attribute("Domain")?.Value ?? ""
                 };
             return credentials.ToArray();
@@ -41,12 +34,6 @@ namespace mRemoteNG.Config.Serializers
             var docSchemaVersion = rootElement?.Attribute("SchemaVersion")?.Value;
             if (docSchemaVersion != SchemaVersion)
                 throw new Exception($"The schema version of this document is not supported by this class. Document Version: {docSchemaVersion} Supported Version: {SchemaVersion}");
-        }
-
-        private void Authenticate(XElement rootElement, ICryptographyProvider cryptographyProvider, SecureString key)
-        {
-            var authString = rootElement.Attribute("Auth")?.Value;
-            cryptographyProvider.Decrypt(authString, key);
         }
     }
 }
