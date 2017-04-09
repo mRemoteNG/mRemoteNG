f40be696c7a60299a1ee242f2675fe155b7bd48f
David Sparer <sparerd@gmail.com>
2017-04-09 17:35:52 -0600

    made utility for creating test connection tree models

--- mRemoteNGTests/Config/Serializers/DataTableSerializerTests.cs
+++ mRemoteNGTests/Config/Serializers/DataTableSerializerTests.cs
@@ -4,6 +4,7 @@ using mRemoteNG.Container;
 using mRemoteNG.Security;
 using mRemoteNG.Tree;
 using mRemoteNG.Tree.Root;
+using mRemoteNGTests.TestHelpers;
 using NUnit.Framework;
 
 namespace mRemoteNGTests.Config.Serializers
@@ -120,17 +121,8 @@ namespace mRemoteNGTests.Config.Serializers
 
         private ConnectionTreeModel CreateConnectionTreeModel()
         {
-            var model = new ConnectionTreeModel();
-            var root = new RootNodeInfo(RootNodeType.Connection);
-            var folder1 = new ContainerInfo {Name = "folder1", Username = "user1", Domain = "domain1", Password = "password1"};
-            var con1 = new ConnectionInfo {Name = "Con1", Username = "user1", Domain = "domain1", Password = "password1" };
-            var con2 = new ConnectionInfo {Name = "Con2", Username = "user2", Domain = "domain2", Password = "password2" };
-
-            root.AddChild(folder1);
-            root.AddChild(con2);
-            folder1.AddChild(con1);
-            model.AddRootNode(root);
-            return model;
+            var builder = new ConnectionTreeModelBuilder();
+            return builder.Build();
         }
     }
 }
---
+++ mRemoteNGTests/TestHelpers/ConnectionTreeModelBuilder.cs
@@ -0,0 +1,25 @@
+﻿using mRemoteNG.Connection;
+using mRemoteNG.Container;
+using mRemoteNG.Tree;
+using mRemoteNG.Tree.Root;
+
+namespace mRemoteNGTests.TestHelpers
+{
+    public class ConnectionTreeModelBuilder
+    {
+        public ConnectionTreeModel Build()
+        {
+            var model = new ConnectionTreeModel();
+            var root = new RootNodeInfo(RootNodeType.Connection);
+            var folder1 = new ContainerInfo { Name = "folder1", Username = "user1", Domain = "domain1", Password = "password1" };
+            var con1 = new ConnectionInfo { Name = "Con1", Username = "user1", Domain = "domain1", Password = "password1" };
+            var con2 = new ConnectionInfo { Name = "Con2", Username = "user2", Domain = "domain2", Password = "password2" };
+
+            root.AddChild(folder1);
+            root.AddChild(con2);
+            folder1.AddChild(con1);
+            model.AddRootNode(root);
+            return model;
+        }
+    }
+}
