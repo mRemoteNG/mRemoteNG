5bfc67af4f533602a0cd50921df17b55728d253a
Sean Kaim <sean.kaim@gmail.com>
2017-04-21 17:20:23 -0400

    Let's get some more clear validation

--- Tools/verify_LargeAddressAware.ps1
+++ Tools/verify_LargeAddressAware.ps1
@@ -16,6 +16,17 @@ $path_dumpBin = & "$PSScriptRoot\find_vstool.ps1" -FileName "dumpbin.exe"
 $path_outputExe = Join-Path -Path $TargetDir -ChildPath $TargetFileName
 
 # Dump exe header
-& $path_dumpBin /NOLOGO /HEADERS "$path_outputExe"
+$output = & "$path_dumpBin" /NOLOGO /HEADERS $path_outputExe | Select-String large
+
+if ($output -eq $null)
+{
+    Write-Warning "Could not validate LargeAddressAware"
+}
+else
+{
+    Write-Output $output.ToString().TrimStart(" ")
+}
+
+
 
 Write-Output ""
