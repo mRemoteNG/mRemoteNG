57fe2a6cc280404c96764c916c34299551674470
David Sparer <David.Sparer@pearson.com>
2017-05-04 10:20:01 -0600

    added some tests for the cred record converter

---
+++ mRemoteNGTests/Credential/CredentialRecordTypeConverterTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using mRemoteNG.Credential;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace mRemoteNGTests.Credential
+{
+    public class CredentialRecordTypeConverterTests
+    {
+        private CredentialRecordTypeConverter _converter;
+
+        [SetUp]
+        public void Setup()
+        {
+            _converter = new CredentialRecordTypeConverter();
+        }
+
+        [TestCase(typeof(Guid), true)]
+        public void CanConvertFrom(Type typeToConvertFrom, bool expectedOutcome)
+        {
+            var actualOutcome = _converter.CanConvertFrom(typeToConvertFrom);
+            Assert.That(actualOutcome, Is.EqualTo(expectedOutcome));
+        }
+
+        [TestCase(typeof(Guid), true)]
+        [TestCase(typeof(ICredentialRecord), true)]
+        public void CanConvertTo(Type typeToConvertFrom, bool expectedOutcome)
+        {
+            var actualOutcome = _converter.CanConvertTo(typeToConvertFrom);
+            Assert.That(actualOutcome, Is.EqualTo(expectedOutcome));
+        }
+
+        [Test]
+        public void ConvertingToGuidReturnsCorrectValue()
+        {
+            var credRecord = Substitute.For<ICredentialRecord>();
+            credRecord.Id.Returns(Guid.NewGuid());
+            var convertedValue = _converter.ConvertTo(credRecord, typeof(Guid));
+            Assert.That(convertedValue, Is.EqualTo(credRecord.Id));
+        }
+    }
+}
