179c3725ca4b6e32a706ba908830b79d746ee2d2
David Sparer <sparerd@gmail.com>
2017-05-07 11:29:22 -0600

    added a tab control that hides its tabs during runtime (shown during design time)

---
+++ mRemoteV1/UI/Controls/HeadlessTabControl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+using mRemoteNG.App;
+
+namespace mRemoteNG.UI.Controls
+{
+    public class HeadlessTabControl : TabControl
+    {
+        protected override void WndProc(ref Message m)
+        {
+            // Hide tabs by trapping the TCM_ADJUSTRECT message
+            if (m.Msg == NativeMethods.TCM_ADJUSTRECT && !DesignMode)
+                m.Result = (IntPtr)1;
+            else
+                base.WndProc(ref m);
+        }
+    }
+}
