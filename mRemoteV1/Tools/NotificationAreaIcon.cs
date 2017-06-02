d88be9aca4df18e64a54470cc870d3ebd52384d4
Sean Kaim <sean.kaim@gmail.com>
2017-06-02 16:48:52 -0400

    Quick Connect from ntf area icon displays warning
    warning occurs when clicking on a folder.
    
    Fixes #546

--- mRemoteV1/Tools/NotificationAreaIcon.cs
+++ mRemoteV1/Tools/NotificationAreaIcon.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Windows.Forms;
 using mRemoteNG.App;
 using mRemoteNG.Connection;
+using mRemoteNG.Container;
 using mRemoteNG.UI.Forms;
 
 
@@ -115,7 +116,7 @@ namespace mRemoteNG.Tools
         private void ConMenItem_MouseUp(object sender, MouseEventArgs e)
         {
             if (e.Button != MouseButtons.Left) return;
-            if (!(((ToolStripMenuItem) sender).Tag is ConnectionInfo)) return;
+            if (((ToolStripMenuItem)sender).Tag is ContainerInfo) return;
             if (FrmMain.Visible == false)
                 ShowForm();
             _connectionInitiator.OpenConnection((ConnectionInfo) ((ToolStripMenuItem) sender).Tag);
