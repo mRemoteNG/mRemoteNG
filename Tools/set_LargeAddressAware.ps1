9335c4706b2ae8d5fa61e418bafbcb5ad653ca9a
Sean Kaim <sean.kaim@gmail.com>
2017-09-15 16:52:42 -0400

    fix editbin post build on jenkins

--- Tools/set_LargeAddressAware.ps1
+++ Tools/set_LargeAddressAware.ps1
@@ -11,16 +11,33 @@ param (
 Write-Output "===== Beginning $($PSCmdlet.MyInvocation.MyCommand) ====="
 
 # Find editbin.exe
-$path_editBin =  @((gci -Path "C:\Program File*\Microsoft Visual Studio\" -Filter editbin.exe -Recurse).FullName)[0]
+#Resolve-Path tends to be faster, but since editbin's are all over the place it's not 100% effective
+$editBinPath = @((Resolve-Path -Path "C:\Program Files*\Microsoft Visual Studio*\VC\bin\editbin.exe").Path)
 
+if(!$editBinPath)
+{
+	# This should work on all VS versions, but doesn't on our Jenkin's build for some reason...
+	# This is needed VC Community
+	$editBinPath =  @((gci -Path "C:\Program*\Microsoft Visual Studio\" -Filter editbin.exe -Recurse)[0].FullName)
+}
+
+# if we STILL can't find it, just return. Same end result NUnit test will fail.
+if(!$editBinPath)
+{
+	echo "Could not find editbin.exe - Can't set LargeAddressAware"
+	return
+}
+
+echo "editBinPath value:"
+echo $editBinPath
 # Verify editbin certificate
-& "$PSScriptRoot\validate_microsoft_tool.ps1" -Path $path_editBin
+& "$PSScriptRoot\validate_microsoft_tool.ps1" -FullPath "$editBinPath"
 
 
-$path_outputExe = Join-Path -Path $TargetDir -ChildPath $TargetFileName
+$outputExe = Join-Path -Path $TargetDir -ChildPath $TargetFileName
 
 # Set LargeAddressAware
-Write-Output "Setting LargeAddressAware on binary file `"$path_outputExe`""
-& $path_editBin "/largeaddressaware" "$path_outputExe"
+Write-Output "Setting LargeAddressAware on binary file `"$outputExe`""
+& $editBinPath "/largeaddressaware" "$outputExe"
 
 Write-Output ""
