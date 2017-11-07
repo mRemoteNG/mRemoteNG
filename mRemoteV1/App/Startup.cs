mRemoteNG/mRemoteNG

commit f73c9c9d0281260fbac61ab409c7a3285becc02f

Refactored cons param, corrected fallback override on first initialization.
Fixes 676

mRemoteV1/App/Startup.cs
@@ -218,6 +218,30 @@ namespace mRemoteNG.App
             }
         }
 
+        /// <summary>
+        /// Returns a path to a file connections XML file for a given absolute or relative path
+        /// </summary>
+        /// <param name="ConsParam">The absolute or relative path to the connection XML file</param>
+        /// <returns>string or null</returns>
+        private static string GetCustomConsPath(string ConsParam)
+        {
+            // early exit condition
+            if (string.IsNullOrEmpty(ConsParam))
+                return null;
+
+            // fallback paths
+            if (File.Exists(ConsParam))
+                return ConsParam;
+
+            if (File.Exists(GeneralAppInfo.HomePath + Path.DirectorySeparatorChar + ConsParam))
+                return GeneralAppInfo.HomePath + Path.DirectorySeparatorChar + ConsParam;
+
+            if (File.Exists(ConnectionsFileInfo.DefaultConnectionsPath + Path.DirectorySeparatorChar + ConsParam))
+                return ConnectionsFileInfo.DefaultConnectionsPath + Path.DirectorySeparatorChar + ConsParam;
+
+            // default case
+            return null;
+        }
 
         private static void ParseCommandLineArgs()
         {
@@ -282,29 +306,13 @@ namespace mRemoteNG.App
                     NoReconnectParam = "norc";
                 }
 
-                if (!string.IsNullOrEmpty(ConsParam))
+                // Handle custom connection file location
+                Settings.Default.CustomConsPath = GetCustomConsPath(ConsParam);
+                if (Settings.Default.CustomConsPath != null)
                 {
-                    if (File.Exists(cmd[ConsParam]))
-                    {
-                        Settings.Default.LoadConsFromCustomLocation = true;
-                        Settings.Default.CustomConsPath = cmd[ConsParam];
-                        return;
-                    }
-                    else
-                    {
-                        if (File.Exists(GeneralAppInfo.HomePath + "\\" + cmd[ConsParam]))
-                        {
-                            Settings.Default.LoadConsFromCustomLocation = true;
-                            Settings.Default.CustomConsPath = GeneralAppInfo.HomePath + "\\" + cmd[ConsParam];
-                            return;
-                        }
-                        if (File.Exists(ConnectionsFileInfo.DefaultConnectionsPath + "\\" + cmd[ConsParam]))
-                        {
-                            Settings.Default.LoadConsFromCustomLocation = true;
-                            Settings.Default.CustomConsPath = ConnectionsFileInfo.DefaultConnectionsPath + "\\" + cmd[ConsParam];
-                            return;
-                        }
-                    }
+                    Settings.Default.LoadConsFromCustomLocation = true;
+                    Settings.Default.Save();
+                    return;
                 }
 
                 if (!string.IsNullOrEmpty(ResetPosParam))

commit afc410cfe6d84f4509d6c40a9fcbe0a9121ff677

fixed #778
custom cons param bug introduced in commit f73c9c9d

mRemoteV1/App/Startup.cs
@@ -253,11 +253,11 @@ namespace mRemoteNG.App
                 var ConsParam = "";
                 if (cmd["cons"] != null)
                 {
-                    ConsParam = "cons";
+                    ConsParam = cmd["cons"];
                 }
                 if (cmd["c"] != null)
                 {
-                    ConsParam = "c";
+                    ConsParam = cmd["c"];
                 }
 
                 var ResetPosParam = "";