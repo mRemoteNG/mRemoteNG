e9d47f046dff8df8cc5453839078cd85ecac941a
David Sparer <sparerd@gmail.com>
2017-11-12 11:39:15 -0600

    removed code for saving to .vre format
    we havent supported this for a long time

--- mRemoteV1/Config/Connections/ConnectionsSaver.cs
+++ mRemoteV1/Config/Connections/ConnectionsSaver.cs
@@ -3,7 +3,6 @@ using System.Data.SqlClient;
 using System.Globalization;
 using System.Linq;
 using System.Security;
-using System.Text;
 using System.Windows.Forms;
 using System.Xml;
 using mRemoteNG.App;
@@ -12,9 +11,6 @@ using mRemoteNG.Config.DatabaseConnectors;
 using mRemoteNG.Config.DataProviders;
 using mRemoteNG.Config.Serializers;
 using mRemoteNG.Config.Serializers.Versioning;
-using mRemoteNG.Connection;
-using mRemoteNG.Connection.Protocol;
-using mRemoteNG.Connection.Protocol.RDP;
 using mRemoteNG.Container;
 using mRemoteNG.Messages;
 using mRemoteNG.Security;
@@ -35,14 +31,10 @@ namespace mRemoteNG.Config.Connections
 			None,
 			mRXML,
 			mRCSV,
-			vRDvRE,
 			SQL
 		}
 				
-        #region Private Properties
-		private XmlTextWriter _xmlTextWriter;
 		private SecureString _password = Runtime.EncryptionKey;
-        #endregion
 				
         #region Public Properties
 		public string SQLHost {get; set;}
@@ -68,9 +60,6 @@ namespace mRemoteNG.Config.Connections
 				case Format.mRCSV:
 			        SaveToMremotengFormattedCsv();
                     break;
-				case Format.vRDvRE:
-					SaveToVRE();
-					break;
 				default:
 					SaveToXml();
 					FrmMain.Default.ConnectionsFileName = ConnectionFileName;
@@ -193,185 +182,5 @@ namespace mRemoteNG.Config.Connections
             var csvContent = csvConnectionsSerializer.Serialize(ConnectionTreeModel);
             dataProvider.Save(csvContent);
         }
-				
-        #region vRD VRE
-        // .VRE files are for ASG-Remote Desktop (prevously visionapp Remote Desktop)
-		private void SaveToVRE()
-		{
-			if (Runtime.ConnectionsService.IsConnectionsFileLoaded == false)
-			{
-				return;
-			}
-
-		    var tN = (TreeNode)RootTreeNode.Clone();
-
-		    var tNC = tN.Nodes;
-					
-			_xmlTextWriter = new XmlTextWriter(ConnectionFileName, Encoding.UTF8);
-			_xmlTextWriter.Formatting = Formatting.Indented;
-			_xmlTextWriter.Indentation = 4;
-					
-			_xmlTextWriter.WriteStartDocument();
-					
-			_xmlTextWriter.WriteStartElement("vRDConfig");
-			_xmlTextWriter.WriteAttributeString("Version", "", "2.0");
-					
-			_xmlTextWriter.WriteStartElement("Connections");
-			SaveNodeVRE(tNC);
-			_xmlTextWriter.WriteEndElement();
-					
-			_xmlTextWriter.WriteEndElement();
-			_xmlTextWriter.WriteEndDocument();
-			_xmlTextWriter.Close();
-		}
-				
-		private void SaveNodeVRE(TreeNodeCollection tNC)
-		{
-			foreach (TreeNode node in tNC)
-			{
-				if (((ConnectionInfo)node.Tag).GetTreeNodeType() == TreeNodeType.Connection)
-				{
-                    var curConI = (ConnectionInfo)node.Tag;
-							
-					if (curConI.Protocol == ProtocolType.RDP)
-					{
-						_xmlTextWriter.WriteStartElement("Connection");
-						_xmlTextWriter.WriteAttributeString("Id", "", "");
-								
-						WriteVREitem(curConI);
-								
-						_xmlTextWriter.WriteEndElement();
-					}
-				}
-				else
-				{
-					SaveNodeVRE(node.Nodes);
-				}
-			}
-		}
-				
-		private void WriteVREitem(ConnectionInfo con)
-		{
-			//Name
-			_xmlTextWriter.WriteStartElement("ConnectionName");
-			_xmlTextWriter.WriteValue(con.Name);
-			_xmlTextWriter.WriteEndElement();
-					
-			//Hostname
-			_xmlTextWriter.WriteStartElement("ServerName");
-			_xmlTextWriter.WriteValue(con.Hostname);
-			_xmlTextWriter.WriteEndElement();
-					
-			//Mac Adress
-			_xmlTextWriter.WriteStartElement("MACAddress");
-			_xmlTextWriter.WriteValue(con.MacAddress);
-			_xmlTextWriter.WriteEndElement();
-					
-			//Management Board URL
-			_xmlTextWriter.WriteStartElement("MgmtBoardUrl");
-			_xmlTextWriter.WriteValue("");
-			_xmlTextWriter.WriteEndElement();
-					
-			//Description
-			_xmlTextWriter.WriteStartElement("Description");
-			_xmlTextWriter.WriteValue(con.Description);
-			_xmlTextWriter.WriteEndElement();
-					
-			//Port
-			_xmlTextWriter.WriteStartElement("Port");
-			_xmlTextWriter.WriteValue(con.Port);
-			_xmlTextWriter.WriteEndElement();
-					
-			//Console Session
-			_xmlTextWriter.WriteStartElement("Console");
-			_xmlTextWriter.WriteValue(con.UseConsoleSession);
-			_xmlTextWriter.WriteEndElement();
-					
-			//Redirect Clipboard
-			_xmlTextWriter.WriteStartElement("ClipBoard");
-			_xmlTextWriter.WriteValue(true);
-			_xmlTextWriter.WriteEndElement();
-					
-			//Redirect Printers
-			_xmlTextWriter.WriteStartElement("Printer");
-			_xmlTextWriter.WriteValue(con.RedirectPrinters);
-			_xmlTextWriter.WriteEndElement();
-					
-			//Redirect Ports
-			_xmlTextWriter.WriteStartElement("Serial");
-			_xmlTextWriter.WriteValue(con.RedirectPorts);
-			_xmlTextWriter.WriteEndElement();
-					
-			//Redirect Disks
-			_xmlTextWriter.WriteStartElement("LocalDrives");
-			_xmlTextWriter.WriteValue(con.RedirectDiskDrives);
-			_xmlTextWriter.WriteEndElement();
-					
-			//Redirect Smartcards
-			_xmlTextWriter.WriteStartElement("SmartCard");
-			_xmlTextWriter.WriteValue(con.RedirectSmartCards);
-			_xmlTextWriter.WriteEndElement();
-					
-			//Connection Place
-			_xmlTextWriter.WriteStartElement("ConnectionPlace");
-			_xmlTextWriter.WriteValue("2"); //----------------------------------------------------------
-			_xmlTextWriter.WriteEndElement();
-					
-			//Smart Size
-			_xmlTextWriter.WriteStartElement("AutoSize");
-			_xmlTextWriter.WriteValue(con.Resolution == RdpProtocol.RDPResolutions.SmartSize);
-			_xmlTextWriter.WriteEndElement();
-					
-			//SeparateResolutionX
-			_xmlTextWriter.WriteStartElement("SeparateResolutionX");
-			_xmlTextWriter.WriteValue("1024");
-			_xmlTextWriter.WriteEndElement();
-					
-			//SeparateResolutionY
-			_xmlTextWriter.WriteStartElement("SeparateResolutionY");
-			_xmlTextWriter.WriteValue("768");
-			_xmlTextWriter.WriteEndElement();
-					
-			var resolution = RdpProtocol.GetResolutionRectangle(con.Resolution);
-			if (resolution.Width == 0)
-			{
-				resolution.Width = 1024;
-			}
-			if (resolution.Height == 0)
-			{
-				resolution.Height = 768;
-			}
-					
-			//TabResolutionX
-			_xmlTextWriter.WriteStartElement("TabResolutionX");
-			_xmlTextWriter.WriteValue(resolution.Width);
-			_xmlTextWriter.WriteEndElement();
-					
-			//TabResolutionY
-			_xmlTextWriter.WriteStartElement("TabResolutionY");
-			_xmlTextWriter.WriteValue(resolution.Height);
-			_xmlTextWriter.WriteEndElement();
-					
-			//RDPColorDepth
-			_xmlTextWriter.WriteStartElement("RDPColorDepth");
-			_xmlTextWriter.WriteValue(con.Colors.ToString().Replace("Colors", "").Replace("Bit", ""));
-			_xmlTextWriter.WriteEndElement();
-					
-			//Bitmap Caching
-			_xmlTextWriter.WriteStartElement("BitmapCaching");
-			_xmlTextWriter.WriteValue(con.CacheBitmaps);
-			_xmlTextWriter.WriteEndElement();
-					
-			//Themes
-			_xmlTextWriter.WriteStartElement("Themes");
-			_xmlTextWriter.WriteValue(con.DisplayThemes);
-			_xmlTextWriter.WriteEndElement();
-					
-			//Wallpaper
-			_xmlTextWriter.WriteStartElement("Wallpaper");
-			_xmlTextWriter.WriteValue(con.DisplayWallpaper);
-			_xmlTextWriter.WriteEndElement();
-		}
-        #endregion
 	}
 }
