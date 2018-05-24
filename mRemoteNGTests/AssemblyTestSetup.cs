c3ae438e9c387942e199cea9b56e18923b542583
David Sparer <sparerd@gmail.com>
2018-05-24 17:48:44 -0500

    fix failing test

---
+++ mRemoteNGTests/AssemblyTestSetup.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using NUnit.Framework;
+
+
+// Dont put this in a namespace. Leaving it by itself tells NUnit
+// to run it on assembly load
+[SetUpFixture]
+public class AssemblyTestSetup
+{
+    [OneTimeSetUp]
+    public void AssemblySetup()
+    {
+        // ensure window options set before any test window created
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+    }
+}
--- mRemoteNGTests/UI/Controls/TextBoxExtensionsTestForm.cs
+++ mRemoteNGTests/UI/Controls/TextBoxExtensionsTestForm.cs
@@ -6,7 +6,6 @@ namespace mRemoteNGTests.UI.Controls
     {
         public TextBoxExtensionsTestForm()
         {
-            Application.EnableVisualStyles();
             InitializeComponent();
         }
     }
