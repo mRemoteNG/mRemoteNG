5b64e629c9fa9dc26ddc46d433e98d7fffcfd92b
Sean Kaim <kmscode@users.noreply.github.com>
2018-07-03 11:59:02 -0400

    zip portable directly via powershell
    Trying to fix appveyor artifact build

--- Tools/zip_portable_files.ps1
+++ Tools/zip_portable_files.ps1
@@ -13,13 +13,39 @@ param (
 )
 
 Write-Output "===== Beginning $($PSCmdlet.MyInvocation.MyCommand) ====="
-$path_packageZipScript = Join-Path -Path $SolutionDir -ChildPath "Tools\build-relport.cmd"
 
+Write-Output "Solution Dir: $($SolutionDir)"
+Write-Output "Target Dir: $($TargetDir)"
+Write-Output "Config Name: $($ConfigurationName)"
+
+# Windows Sysinternals Sigcheck from http://technet.microsoft.com/en-us/sysinternals/bb897441
+$SIGCHECK="$($SolutionDir)Tools\exes\sigcheck.exe"
+$SEVENZIP="$($SolutionDir)Tools\7zip\7za.exe"
 
 # Package Zip
-if ($ConfigurationName -match "Release" -and $ConfigurationName -match "Portable") {
+if ($ConfigurationName -eq "Release Portable") {
     Write-Output "Packaging Release Portable ZIP"
-    & $path_packageZipScript
+   
+    $version = & $SIGCHECK /accepteula -q -n "$($SolutionDir)mRemoteV1\bin\$ConfigurationName\mRemoteNG.exe"
+
+    Write-Output "Version is $($version)"
+
+    $PortableZip="$($SolutionDir)Release\mRemoteNG-Portable-$($version).zip"
+
+    Remove-Item -Recurse "$($SolutionDir)mRemoteV1\bin\package" -ErrorAction SilentlyContinue | Out-Null
+    New-Item "$($SolutionDir)mRemoteV1\bin\package" -ItemType  "directory" | Out-Null
+    
+    Copy-Item "$($SolutionDir)mRemoteV1\Resources\PuTTYNG.exe" -Destination "$($SolutionDir)mRemoteV1\bin\package"
+
+    #Write-Output "$($SolutionDir)mRemoteV1\bin\$ConfigurationName" 
+    #Write-Output "$($SolutionDir)mRemoteV1\bin\package"
+    Copy-Item "$($SolutionDir)mRemoteV1\bin\$ConfigurationName\*" -Destination "$($SolutionDir)mRemoteV1\bin\package" -Recurse  -Force 
+    Copy-Item "$($SolutionDir)*.txt" -Destination "$($SolutionDir)mRemoteV1\bin\package"
+
+    Write-Output "Creating portable ZIP file $($PortableZip)"
+    Remove-Item -Force  $PortableZip -ErrorAction SilentlyContinue
+    & $SEVENZIP a -bt -bd -bb1 -mx=9 -tzip -y -r $PortableZip "$($SolutionDir)mRemoteV1\bin\package\*.*"
+    #& $SEVENZIP a -bt -mx=9 -tzip -y $PortableZip "$($SolutionDir)*.TXT"
 }
 else {
     Write-Output "We will not zip anything - this isnt a portable release build."
