930579d98361a7ca23587d6dafb2d298f8e55036
Sean Kaim <kmscode@users.noreply.github.com>
2018-07-25 12:35:53 -0400

    PortableSettingsProvider code clean up

--- mRemoteV1/Config/Settings/Providers/ChooseProvider.cs
+++ mRemoteV1/Config/Settings/Providers/ChooseProvider.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-
-namespace mRemoteNG.Config.Settings.Providers
+﻿namespace mRemoteNG.Config.Settings.Providers
 {
 #if PORTABLE
     public class ChooseProvider : PortableSettingsProvider
