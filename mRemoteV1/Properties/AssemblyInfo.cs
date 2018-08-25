e0c2037831e2b78ee9c5e428e284de7b145d6ae5
David Sparer <sparerd@gmail.com>
2018-08-25 11:38:40 -0500

    set assembly version to 1.77.0

--- mRemoteV1/Properties/AssemblyInfo.cs
+++ mRemoteV1/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")>
 
-[assembly: AssemblyVersion("1.76.8.*")]
+[assembly: AssemblyVersion("1.77.0.*")]
 [assembly: NeutralResourcesLanguage("en")]
