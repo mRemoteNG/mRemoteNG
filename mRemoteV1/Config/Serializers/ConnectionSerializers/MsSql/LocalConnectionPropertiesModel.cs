4aba36b756dbb7dd60922b10867ecf809ba8ee27
David Sparer <sparerd@gmail.com>
2018-11-01 16:37:30 -0500

    added classes to handle local connection properties when dealing with the DB

---
+++ mRemoteV1/Config/Serializers/ConnectionSerializers/MsSql/LocalConnectionPropertiesModel.cs
@@ -0,0 +1,8 @@
+﻿namespace mRemoteNG.Config.Serializers.MsSql
+{
+    public class LocalConnectionPropertiesModel
+    {
+        public string ConnectionId { get; set; }
+        public bool Connected { get; set; }
+    }
+}
