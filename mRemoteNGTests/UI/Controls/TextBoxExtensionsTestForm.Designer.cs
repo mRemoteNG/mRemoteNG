ea5b0362df601a6989e214d76d7f184de1feee83
Sean Kaim <kmscode@users.noreply.github.com>
2019-01-05 14:34:02 -0500

    more DPI scaling fixes + frmChoosePnl fixes
    frmChoosePanel button layout was off a bit.
    Exception when clicking "new" when no panels exist.

--- mRemoteNGTests/UI/Controls/TextBoxExtensionsTestForm.Designer.cs
+++ mRemoteNGTests/UI/Controls/TextBoxExtensionsTestForm.Designer.cs
@@ -40,8 +40,8 @@
             // 
             // TextBoxExtensionsTestForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(220, 48);
             this.Controls.Add(this.textBox1);
             this.Name = "TextBoxExtensionsTestForm";
