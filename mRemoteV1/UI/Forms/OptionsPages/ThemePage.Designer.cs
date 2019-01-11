4e11baa5790c35e3a46735cb82d096f078de3ec4
Sean Kaim <sean.kaim@gmail.com>
2019-01-11 09:03:57 -0500

    designer generated change

--- mRemoteV1/UI/Forms/OptionsPages/ThemePage.Designer.cs
+++ mRemoteV1/UI/Forms/OptionsPages/ThemePage.Designer.cs
@@ -111,6 +111,7 @@ namespace mRemoteNG.UI.Forms.OptionsPages
             this.listPalette.Cursor = System.Windows.Forms.Cursors.Default;
             this.listPalette.DecorateLines = true;
             this.listPalette.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listPalette.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listPalette.Location = new System.Drawing.Point(3, 37);
             this.listPalette.Name = "listPalette";
             this.listPalette.ShowGroups = false;
