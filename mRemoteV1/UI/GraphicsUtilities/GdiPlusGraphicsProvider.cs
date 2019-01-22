1f700f7842496315d69a352422fc7394d90fa5a2
Camilo Alvarez <jotatsu@gmail.com>
2019-01-21 23:29:26 -0500

    Fix for #1257
    CreateGraphics was the function being disposed in the using, the form handle was still floating around. Suggestion to optimize the dpi function

--- mRemoteV1/UI/GraphicsUtilities/GdiPlusGraphicsProvider.cs
+++ mRemoteV1/UI/GraphicsUtilities/GdiPlusGraphicsProvider.cs
@@ -11,12 +11,16 @@ namespace mRemoteNG.UI.GraphicsUtilities
         // Dpi of a 'normal' definition screen
         private const int BaselineDpi = 96;
 
+        
         public SizeF GetResolutionScalingFactor()
         {
-            using (var g = new Form().CreateGraphics())
-            {
-                return new SizeF(g.DpiX / BaselineDpi, g.DpiY / BaselineDpi);
-            }
+            //This method could be optimized, as it is called for every control / subcontrol 
+            //and causes overhead for 100s in the options page
+            using (var f = new Form())
+             {
+                var g = f.CreateGraphics();
+                return  new SizeF(g.DpiX / BaselineDpi, g.DpiY / BaselineDpi); 
+             }
         }
     }
 }
