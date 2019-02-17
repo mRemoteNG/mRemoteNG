09c8c97b4ecfc3bd24f84a7e9b751a91c2f6e5d1
Mike Beattie <mike@ethernal.org>
2019-02-17 14:05:31 +1300

    Update ConnectionFileVersion to match SQL version
    Also add missing columns to SQL script in Help for 2.7
    
    Signed-off-by: Mike Beattie <mike@ethernal.org>

--- mRemoteV1/App/Info/ConnectionsFileInfo.cs
+++ mRemoteV1/App/Info/ConnectionsFileInfo.cs
@@ -5,6 +5,6 @@
         public static readonly string DefaultConnectionsPath = SettingsFileInfo.SettingsPath;
         public static readonly string DefaultConnectionsFile = "confCons.xml";
         public static readonly string DefaultConnectionsFileNew = "confConsNew.xml";
-        public static readonly double ConnectionFileVersion = 2.6;
+        public static readonly double ConnectionFileVersion = 2.7;
     }
 }
