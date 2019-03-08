738b159e95d41cc19d211bfdad5565d42721afe5
David Sparer <sparerd@gmail.com>
2019-03-08 10:31:39 -0600

    handle sql first-run case
    fixes #1303

--- mRemoteV1/Config/Connections/SqlConnectionsLoader.cs
+++ mRemoteV1/Config/Connections/SqlConnectionsLoader.cs
@@ -1,23 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
 using mRemoteNG.Config.DatabaseConnectors;
 using mRemoteNG.Config.DataProviders;
 using mRemoteNG.Config.Serializers;
 using mRemoteNG.Config.Serializers.MsSql;
 using mRemoteNG.Config.Serializers.Versioning;
 using mRemoteNG.Container;
-using mRemoteNG.Tools;
-using mRemoteNG.Tree;
-using mRemoteNG.Tree.Root;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security;
 using mRemoteNG.Security;
 using mRemoteNG.Security.Authentication;
 using mRemoteNG.Security.SymmetricEncryption;
+using mRemoteNG.Tools;
+using mRemoteNG.Tree;
+using mRemoteNG.Tree.Root;
 
 namespace mRemoteNG.Config.Connections
 {
-    public class SqlConnectionsLoader : IConnectionsLoader
+	public class SqlConnectionsLoader : IConnectionsLoader
     {
         private readonly IDeserializer<string, IEnumerable<LocalConnectionPropertiesModel>> _localConnectionPropertiesDeserializer;
         private readonly IDataProvider<string> _dataProvider;
@@ -41,7 +41,8 @@ namespace mRemoteNG.Config.Connections
             var databaseVersionVerifier = new SqlDatabaseVersionVerifier(connector);
             var cryptoProvider = new LegacyRijndaelCryptographyProvider();
 
-            var metaData = metaDataRetriever.GetDatabaseMetaData(connector);
+            var metaData = metaDataRetriever.GetDatabaseMetaData(connector) ?? 
+                           HandleFirstRun(metaDataRetriever, connector);
             var decryptionKey = GetDecryptionKey(metaData);
 
             if (!decryptionKey.Any())
@@ -85,5 +86,11 @@ namespace mRemoteNG.Config.Connections
                         container.IsExpanded = x.LocalProperties.Expanded;
                 });
         }
+
+        private SqlConnectionListMetaData HandleFirstRun(SqlDatabaseMetaDataRetriever metaDataRetriever, SqlDatabaseConnector connector)
+        {
+	        metaDataRetriever.WriteDatabaseMetaData(new RootNodeInfo(RootNodeType.Connection), connector);
+	        return metaDataRetriever.GetDatabaseMetaData(connector);
+		}
     }
 }
