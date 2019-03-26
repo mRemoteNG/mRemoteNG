1460f403d6b855ef24aa230191849b7d200993fa
Faryan Rezagholi <faryan.rezagholi@gmail.com>
2019-03-26 19:40:12 +0100

    retired "move_help_files" post build script

--- Tools/sphinx_docs.ps1
+++ Tools/sphinx_docs.ps1
@@ -1,21 +1,23 @@
-param (
+param (	
 	[string]
     [Parameter(Mandatory=$true)]
-    $SolutionDir
+    $SolutionDir,
+	
+	[string]
+    [Parameter(Mandatory=$true)]
+    $TargetDir
 )
 
 Write-Output "===== Beginning $($PSCmdlet.MyInvocation.MyCommand) ====="
 
-$path_SphinxSourceDir = Join-Path -Path $SolutionDir -ChildPath "mremoteV1\Documentation\"
-$path_SphinxBuildDir = Join-Path -Path $SolutionDir -ChildPath "mremoteV1\Documentation\_build\"
-#$buildScript = "$($path_SphinxDocsDir)\make.bat"
-
 Write-Output "Building HTML-Documentation with Sphinx"
 
-sphinx-build $path_SphinxSourceDir $path_SphinxBuildDir
-#Start-Process $buildScript -NoNewWindow -Wait
+$path_HelpFilesDir = Join-Path -Path $TargetDir -ChildPath "Help"
+$path_SphinxSourceDir = Join-Path -Path $SolutionDir -ChildPath "mremoteV1\Documentation"
+# Remove stale Help files, if they exist
+if (Test-Path -Path $path_HelpFilesDir) {
+    Remove-Item -Path $path_HelpFilesDir -Recurse -Force
+}
+sphinx-build $path_SphinxSourceDir $path_HelpFilesDir
 
 Write-Output ""
-
-
-#sphinx-build C:\Source\mRemoteNG\mRemoteV1\Documentation\ C:\Source\mRemoteNG\mRemoteV1\Documentation\_build\
