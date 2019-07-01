5cab342c8c85b5d2d199ddd20816cfae3c09b94c
Faryan Rezagholi <faryan@rezagholi.de>
2019-07-01 22:24:46 +0200

    do not try to show an exception windows when main form is already disposed

--- mRemoteV1/App/ProgramRoot.cs
+++ mRemoteV1/App/ProgramRoot.cs
@@ -88,8 +88,11 @@ namespace mRemoteNG.App
             if (!FrmSplashScreen.getInstance().IsDisposed)
                 FrmSplashScreen.getInstance().Close();
 
+            if (FrmMain.Default.IsDisposed) return;
+            
             var window = new UnhandledExceptionWindow(e.Exception, false);
             window.ShowDialog(FrmMain.Default);
+            
         }
 
         private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
