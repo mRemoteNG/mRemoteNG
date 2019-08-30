125330b695764e454a71d4982c16b097fbb6ff76
david-sway <54483158+david-sway@users.noreply.github.com>
2019-08-30 09:15:45 -0400

    Added handling of Gecko.LauncherDialog.Download (#1548)
    * Added handling of Gecko.LauncherDialog.Download
    Code lifted from https://stackoverflow.com/questions/27368791/how-to-handle-downloading-in-geckofx-29
    This code addition addresses https://github.com/mRemoteNG/mRemoteNG/issues/1400
    * updated changelog for #1400
    * refractored launcher dialog method

--- mRemoteV1/Connection/Protocol/Http/Connection.Protocol.HTTPBase.cs
+++ mRemoteV1/Connection/Protocol/Http/Connection.Protocol.HTTPBase.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 using Gecko;
 using mRemoteNG.Tools;
@@ -66,6 +67,7 @@ namespace mRemoteNG.Connection.Protocol.Http
                     if (GeckoBrowser != null)
                     {
                         GeckoBrowser.DocumentTitleChanged += geckoBrowser_DocumentTitleChanged;
+                        LauncherDialog.Download += geckoBrowser_LauncherDialog_Download;
                         GeckoBrowser.NSSError += CertEvent.GeckoBrowser_NSSError;
                     }
                     else
@@ -265,6 +267,40 @@ namespace mRemoteNG.Connection.Protocol.Http
             {
                 Runtime.MessageCollector.AddExceptionStackTrace(Language.strHttpDocumentTileChangeFailed, ex);
             }
+        }
+
+        private void geckoBrowser_LauncherDialog_Download(object sender, Gecko.LauncherDialogEvent e)
+        {
+            var objTarget = Xpcom.CreateInstance<nsILocalFile>("@mozilla.org/file/local;1");
+            using (var tmp = new nsAString(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\mremoteng.download"))
+            {
+                objTarget.InitWithPath(tmp);
+            }
+
+            //Save file dialog
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "All files (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+                FileName = e.Filename
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var source = IOService.CreateNsIUri(e.Url);
+                var dest = IOService.CreateNsIUri(new Uri(saveFileDialog.FileName).AbsoluteUri);
+                var t = (nsAStringBase)new nsAString(Path.GetFileName(saveFileDialog.FileName));
+                var persist = Xpcom.CreateInstance<nsIWebBrowserPersist>("@mozilla.org/embedding/browser/nsWebBrowserPersist;1");
+                var nst = Xpcom.CreateInstance<nsITransfer>("@mozilla.org/transfer;1");
+
+                nst.Init(source, dest, t, e.Mime, 0, null, persist, false);
+                persist.SetPersistFlagsAttribute(2 | 32 | 16384);
+                persist.SetProgressListenerAttribute(nst);
+                persist.SaveURI(source, null, null, (uint)Gecko.nsIHttpChannelConsts.REFERRER_POLICY_NO_REFERRER, null, null, (nsISupports)dest, null);
+            }
+
+            saveFileDialog.Dispose();
         }
 
         #endregion
