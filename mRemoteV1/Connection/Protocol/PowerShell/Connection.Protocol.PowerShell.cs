de3072c67eee5c910a8a1ffac166cdc0c729adda
Faryan Rezagholi <faryan.rezagholi@gmail.com>
2019-09-08 13:02:58 +0200

    Added ConsoleControl, since it seems that the powershell.exe cannot be integrated

--- mRemoteV1/Connection/Protocol/PowerShell/Connection.Protocol.PowerShell.cs
+++ mRemoteV1/Connection/Protocol/PowerShell/Connection.Protocol.PowerShell.cs
@@ -6,7 +6,6 @@ using System.Threading;
 using System.Windows.Forms;
 using mRemoteNG.App;
 using mRemoteNG.Messages;
-using mRemoteNG.Tools;
 
 namespace mRemoteNG.Connection.Protocol.PowerShell
 {
@@ -15,8 +14,8 @@ namespace mRemoteNG.Connection.Protocol.PowerShell
         #region Private Fields
 
         private IntPtr _handle;
-        private Process _process;
         private readonly ConnectionInfo _connectionInfo;
+        private ConsoleControl.ConsoleControl _consoleControl;
 
         public ProtocolPowerShell(ConnectionInfo connectionInfo)
         {
@@ -33,27 +32,24 @@ namespace mRemoteNG.Connection.Protocol.PowerShell
             {
                 Runtime.MessageCollector?.AddMessage(MessageClass.InformationMsg,
                                                      $"Attempting to start remote PowerShell session.", true);
-                
-                _process = new Process
+
+                _consoleControl = new ConsoleControl.ConsoleControl
                 {
-                    StartInfo =
-                    {
-                        UseShellExecute = true,
-                        FileName = @"C:\Windows\system32\WindowsPowerShell\v1.0\PowerShell.exe",
-                        Arguments = $@"-NoExit -Command ""$password = ConvertTo-SecureString '{_connectionInfo.Password}' -AsPlainText -Force; $cred = New-Object System.Management.Automation.PSCredential -ArgumentList @('{_connectionInfo.Domain}\{_connectionInfo.Username}', $password); Enter-PSSession -ComputerName {_connectionInfo.Hostname} -Credential $cred"""
-                    },
-                    EnableRaisingEvents = true,
+                    Dock = DockStyle.Fill,
+                    BackColor = Color.DarkBlue,
+                    ForeColor = Color.White,
+                    IsInputEnabled = true,
+                    Padding = new Padding(0, 20, 0, 0)
                 };
+
+                _consoleControl.StartProcess(@"C:\Windows\system32\WindowsPowerShell\v1.0\PowerShell.exe",
+                    $@"-NoExit -Command ""$password = ConvertTo-SecureString '{_connectionInfo.Password}' -AsPlainText -Force; $cred = New-Object System.Management.Automation.PSCredential -ArgumentList @('{_connectionInfo.Domain}\{_connectionInfo.Username}', $password); Enter-PSSession -ComputerName {_connectionInfo.Hostname} -Credential $cred""");
                 
-                _process.Exited += ProcessExited;
-                _process.Start();
-                _process.WaitForInputIdle();
-
-                NativeMethods.SetParent(_process.Handle, InterfaceControl.Handle);
-
-
+                while (!_consoleControl.IsHandleCreated) break;
+                _handle = _consoleControl.Handle;
+                NativeMethods.SetParent(_handle, InterfaceControl.Handle);
                 
-                //Resize(this, new EventArgs());
+                Resize(this, new EventArgs());
                 base.Connect();
                 return true;
             }
@@ -63,9 +59,6 @@ namespace mRemoteNG.Connection.Protocol.PowerShell
                 return false;
             }
         }
-
-        [DllImport("user32.dll")]
-        static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
 
         public override void Focus()
         {
@@ -94,51 +87,6 @@ namespace mRemoteNG.Connection.Protocol.PowerShell
             {
                 Runtime.MessageCollector.AddExceptionMessage(Language.strIntAppResizeFailed, ex);
             }
-        }
-
-        public override void Close()
-        {
-            /* only attempt this if we have a valid process object
-             * Non-integrated tools will still call base.Close() and don't have a valid process object.
-             * See Connect() above... This just muddies up the log.
-             */
-            if (_process != null)
-            {
-                try
-                {
-                    if (!_process.HasExited)
-                    {
-                        _process.Kill();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Runtime.MessageCollector.AddExceptionMessage(Language.strIntAppKillFailed, ex);
-                }
-
-                try
-                {
-                    if (!_process.HasExited)
-                    {
-                        _process.Dispose();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Runtime.MessageCollector.AddExceptionMessage(Language.strIntAppDisposeFailed, ex);
-                }
-            }
-
-            base.Close();
-        }
-
-        #endregion
-
-        #region Private Methods
-
-        private void ProcessExited(object sender, EventArgs e)
-        {
-            Event_Closed(this);
         }
 
         #endregion
