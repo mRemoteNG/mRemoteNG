7ce17c7fb3ce63ce7e6abb0809cae327c27cdefc
Faryan Rezagholi <faryan.rezagholi@gmail.com>
2019-09-08 15:27:56 +0200

    added powershell connection icon

--- mRemoteV1/Properties/Resources.Designer.cs
+++ mRemoteV1/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace mRemoteNG {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -1042,6 +1042,16 @@ namespace mRemoteNG {
         internal static System.Drawing.Icon PortScan_Icon {
             get {
                 object obj = ResourceManager.GetObject("PortScan_Icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon PowerShell {
+            get {
+                object obj = ResourceManager.GetObject("PowerShell", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
             }
         }
