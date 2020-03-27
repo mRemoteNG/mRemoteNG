829badaf45972eb58478827b19b5164e6df8b6b8
hummigbird1 <hummigbird1@gmail.com>
2020-03-27 11:54:27 +0100

    Extract common Interface for ICA Client object and implement dummy instance

---
+++ mRemoteV1/Connection/Protocol/ICA/DummyICAClient.cs
@@ -0,0 +1,89 @@
+﻿#if DEBUG
+using mRemoteNG.UI.Window;
+using System;
+using System.Windows.Forms;
+
+namespace mRemoteNG.Connection.Protocol.ICA
+{
+    public class DummyICAClient : IICAClient
+    {
+        public DummyICAClient()
+        {
+            throw new NotImplementedException("This version has been build with ICA Protocol disabled via compile constant 'DISABLE_ICA_PROTOCOL' this should not have happened in a release version!");
+        }
+
+        public event EventHandler<EventArgs> OnConnect;
+
+        public event EventHandler<EventArgs> OnConnectFailed;
+
+        public event EventHandler<EventArgs> OnConnecting;
+
+        public event EventHandler<EventArgs> OnDisconnect;
+
+        public string Address { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Control Control => throw new NotImplementedException();
+
+        public bool Created => throw new NotImplementedException();
+        public string Domain { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool Encrypt { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string EncryptionLevelSession { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey10Char { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey10Shift { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey11Char { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey11Shift { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey1Char { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey1Shift { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey2Char { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey2Shift { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey3Char { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey3Shift { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey4Char { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey4Shift { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey5Char { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey5Shift { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey6Char { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey6Shift { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey7Char { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey7Shift { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey8Char { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey8Shift { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey9Char { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Hotkey9Shift { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ComponentsCheckWindow Parent { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool PersistentCacheEnabled { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Title { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Username { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Version => throw new NotImplementedException();
+
+        public void Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Control CreateControl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FullScreenWindow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetProp(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetWindowSize(int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+#endif
---
+++ mRemoteV1/Connection/Protocol/ICA/IICAClient.cs
@@ -0,0 +1,61 @@
+﻿using mRemoteNG.UI.Window;
+using System;
+using System.Windows.Forms;
+
+namespace mRemoteNG.Connection.Protocol.ICA
+{
+    public interface IICAClient : IDisposable
+    {
+        event EventHandler<EventArgs> OnConnect;
+
+        event EventHandler<EventArgs> OnConnectFailed;
+
+        event EventHandler<EventArgs> OnConnecting;
+
+        event EventHandler<EventArgs> OnDisconnect;
+
+        string Address { get; set; }
+        Control Control { get; }
+        bool Created { get; }
+        string Domain { get; set; }
+        bool Encrypt { get; set; }
+        string EncryptionLevelSession { get; set; }
+        object Hotkey10Char { get; set; }
+        object Hotkey10Shift { get; set; }
+        object Hotkey11Char { get; set; }
+        object Hotkey11Shift { get; set; }
+        object Hotkey1Char { get; set; }
+        object Hotkey1Shift { get; set; }
+        object Hotkey2Char { get; set; }
+        object Hotkey2Shift { get; set; }
+        object Hotkey3Char { get; set; }
+        object Hotkey3Shift { get; set; }
+        object Hotkey4Char { get; set; }
+        object Hotkey4Shift { get; set; }
+        object Hotkey5Char { get; set; }
+        object Hotkey5Shift { get; set; }
+        object Hotkey6Char { get; set; }
+        object Hotkey6Shift { get; set; }
+        object Hotkey7Char { get; set; }
+        object Hotkey7Shift { get; set; }
+        object Hotkey8Char { get; set; }
+        object Hotkey8Shift { get; set; }
+        object Hotkey9Char { get; set; }
+        object Hotkey9Shift { get; set; }
+        ComponentsCheckWindow Parent { get; set; }
+        bool PersistentCacheEnabled { get; set; }
+        string Title { get; set; }
+        string Username { get; set; }
+        object Version { get; }
+
+        void Connect();
+
+        Control CreateControl();
+
+        void FullScreenWindow();
+
+        void SetProp(string name, string value);
+
+        void SetWindowSize(int width, int height);
+    }
+}
