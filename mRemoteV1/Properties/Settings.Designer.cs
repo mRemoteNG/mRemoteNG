c99fe4b0efae22c17b366ea7042b4bcfe1de5c6f
Faryan Rezagholi <faryan.rezagholi@gmail.com>
2020-05-23 23:22:00 +0200

    removed oboslete settings for rendering engine

--- mRemoteV1/Properties/Settings.Designer.cs
+++ mRemoteV1/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace mRemoteNG {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -1556,30 +1556,6 @@ namespace mRemoteNG {
             }
             set {
                 this["XULRunnerPath"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("IE")]
-        public string ConDefaultRenderingEngine {
-            get {
-                return ((string)(this["ConDefaultRenderingEngine"]));
-            }
-            set {
-                this["ConDefaultRenderingEngine"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
-        public bool InhDefaultRenderingEngine {
-            get {
-                return ((bool)(this["InhDefaultRenderingEngine"]));
-            }
-            set {
-                this["InhDefaultRenderingEngine"] = value;
             }
         }
         
