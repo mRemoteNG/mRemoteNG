8ea8c411d63e16e5f4942fa037bf3af9c74a9d35
Faryan Rezagholi <faryan@rezagholi.de>
2020-06-02 09:11:01 +0200

    renamed/moved project files

---
+++ mRemoteNGSpecs/Utilities/CredRepoXmlFileBuilder.cs
@@ -0,0 +1,12 @@
+﻿namespace mRemoteNGSpecs.Utilities
+{
+    public class CredRepoXmlFileBuilder
+    {
+        public string Build(string authHeader)
+        {
+            return "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                    $"<Credentials EncryptionEngine=\"AES\" BlockCipherMode=\"GCM\" KdfIterations=\"1000\" Auth=\"{authHeader}\" SchemaVersion=\"1.0\">" +
+                    "</Credentials>";
+        }
+    }
+}
