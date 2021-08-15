0e74314ddc647d177dbc670788ecb45864336910
Faryan Rezagholi <faryan.rezagholi@siedle.de>
2021-08-15 21:53:10 +0200

    fixed port scan and external tools window not showing

--- mRemoteNG/UI/Window/PortScanWindow.Designer.cs
+++ mRemoteNG/UI/Window/PortScanWindow.Designer.cs
@@ -494,7 +494,6 @@ namespace mRemoteNG.UI.Window
             this.ClientSize = new System.Drawing.Size(884, 461);
             this.Controls.Add(this.pnlMain);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "PortScanWindow";
             this.TabText = "Port Scan";
             this.Text = "Port Scan";
