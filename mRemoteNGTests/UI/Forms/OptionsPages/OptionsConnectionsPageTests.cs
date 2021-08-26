f77848e3d97301f2d386b20cb8853e0e344981d4
Ian Dunkerly <iddesigns@gmail.com>
2021-08-26 22:59:35 +0100

    Fixing up unit tests

--- mRemoteNGTests/UI/Forms/OptionsPages/OptionsConnectionsPageTests.cs
+++ mRemoteNGTests/UI/Forms/OptionsPages/OptionsConnectionsPageTests.cs
@@ -1,33 +1,34 @@
-﻿using System;
+﻿using System.Threading;
+using System.Windows.Forms;
+using mRemoteNGTests.TestHelpers;
 using NUnit.Framework;
-using mRemoteNGTests.UI.Forms;
-using NUnit.Extensions.Forms;
 
 namespace mRemoteNGTests.UI.Forms.OptionsPages
 {
     [TestFixture]
+    [Apartment(ApartmentState.STA)]
     public class OptionsConnectionsPageTests : OptionsFormSetupAndTeardown
     {
         [Test]
         public void ConnectionsPageLinkExistsInListView()
         {
-            ListViewTester listViewTester = new ListViewTester("PageListView", _optionsForm);
-            Assert.That(listViewTester.Items[3].Text, Does.Match("Connections"));
+            ListViewTester listViewTester = new ListViewTester("lstOptionPages", _optionsForm);
+            Assert.That(listViewTester.Items[4].Text, Does.Match("Connections"));
         }
 
         [Test]
         public void ConnectionsIconShownInListView()
         {
-            ListViewTester listViewTester = new ListViewTester("PageListView", _optionsForm);
-            Assert.That(listViewTester.Items[3].ImageList, Is.Not.Null);
+            ListViewTester listViewTester = new ListViewTester("lstOptionPages", _optionsForm);
+            Assert.That(listViewTester.Items[4].ImageList, Is.Not.Null);
         }
 
         [Test]
         public void SelectingConnectionsPageLoadsSettings()
         {
-            ListViewTester listViewTester = new ListViewTester("PageListView", _optionsForm);
+            ListViewTester listViewTester = new ListViewTester("lstOptionPages", _optionsForm);
             listViewTester.Select("Connections");
-            CheckBoxTester checkboxTester = new CheckBoxTester("chkSingleClickOnConnectionOpensIt", _optionsForm);
+            CheckBox checkboxTester = _optionsForm.FindControl<CheckBox>("chkSingleClickOnConnectionOpensIt");
             Assert.That(checkboxTester.Text, Does.Match("Single click on connection"));
         }
     }
