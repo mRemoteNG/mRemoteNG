73313d7dae654b1ba621c3688656669ff7678def
Faryan Rezagholi <faryan.rezagholi@siedle.de>
2021-12-09 20:11:45 +0100

    fixes/simplified postbuild installer scripts

---
+++ Tools/rename_and_copy_installer.ps1
@@ -0,0 +1,13 @@
+﻿param (
+    [string]
+    $SolutionDir
+)
+
+
+$targetVersionedFile = "$SolutionDir\mRemoteNG\bin\x64\Release\mRemoteNG.exe"
+$version = &"$SolutionDir\Tools\exes\sigcheck.exe" /accepteula -q -n $targetVersionedFile
+$src = $SolutionDir + "mRemoteNGInstaller\Installer\bin\Release\en-US\mRemoteNG-Installer.msi"
+$dst = $SolutionDir + "Release\mRemoteNG-Installer-" + $version + ".msi"
+
+# Copy file
+Copy-Item $src -Destination $dst -Force
