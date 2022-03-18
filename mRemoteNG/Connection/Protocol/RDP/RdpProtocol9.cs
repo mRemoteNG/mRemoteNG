2fb0ab1d91639a0ca483430d60db02869cad8eae
Daniel Triendl <daniel@pew.cc>
2022-03-18 19:17:46 +0100

    Enable size change without reconnect
    Use IMsRdpClient9::UpdateSessionDisplaySettings to dynamically update the
    session display settings without reconnecting. RDP Version needs to be
    Rdc9 or Highest for this to work.
    
    Fixes #1546

--- mRemoteNG/Connection/Protocol/RDP/RdpProtocol9.cs
+++ mRemoteNG/Connection/Protocol/RDP/RdpProtocol9.cs
@@ -1,15 +1,24 @@
 ﻿using System.Windows.Forms;
 using AxMSTSCLib;
+using MSTSCLib;
 
 namespace mRemoteNG.Connection.Protocol.RDP
 {
     public class RdpProtocol9 : RdpProtocol8
     {
+        private MsRdpClient9NotSafeForScripting RdpClient9 =>
+            (MsRdpClient9NotSafeForScripting)((AxHost)Control).GetOcx();
+
         protected override RdpVersion RdpProtocolVersion => RdpVersion.Rdc9;
 
         protected override AxHost CreateActiveXRdpClientControl()
         {
             return new AxMsRdpClient9NotSafeForScripting();
+        }
+
+        protected override void UpdateSessionDisplaySettings(uint width, uint height)
+        {
+            RdpClient9.UpdateSessionDisplaySettings(width, height, width, height, 0, 1, 1);
         }
     }
 }
