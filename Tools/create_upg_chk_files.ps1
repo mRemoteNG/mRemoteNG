42949fbe91482f4444af4f5ceecac34911ab8e54
BlueBlock <theblueblock@protonmail.com>
2023-02-20 05:21:11 -0500

    update build scripts to create update files

--- Tools/create_upg_chk_files.ps1
+++ Tools/create_upg_chk_files.ps1
@@ -2,6 +2,18 @@
 param (
     [string]
     [Parameter(Mandatory=$true)]
+    $WebsiteTargetOwner,
+
+    [string]
+    [Parameter(Mandatory=$true)]
+    $WebsiteTargetRepository,
+
+    [string]
+    [Parameter(Mandatory=$false)]
+    $PreTagName = "",
+
+    [string]
+    [Parameter(Mandatory=$true)]
     $TagName,
 
     [string]
@@ -23,7 +35,7 @@ function New-MsiUpdateFileContent {
     
     $version = $MsiFile.BaseName -replace "[a-zA-Z-]*"
     $certThumbprint = (Get-AuthenticodeSignature -FilePath $MsiFile).SignerCertificate.Thumbprint
-    $hash = Get-FileHash -Algorithm SHA512 $MsiFile | % { $_.Hash }
+    $hash = Get-FileHash -Algorithm SHA512 $MsiFile | ForEach-Object { $_.Hash }
 
     $fileContents = `
 "Version: $version
@@ -47,7 +59,7 @@ function New-ZipUpdateFileContent {
     )
     
     $version = $ZipFile.BaseName -replace "[a-zA-Z-]*"
-    $hash = Get-FileHash -Algorithm SHA512 $ZipFile | % { $_.Hash }
+    $hash = Get-FileHash -Algorithm SHA512 $ZipFile | ForEach-Object { $_.Hash }
 
     $fileContents = `
 "Version: $version
@@ -85,18 +97,23 @@ function Resolve-UpdateCheckFileName {
     Write-Output $fileName
 }
 
+
 Write-Output "Begin create_upg_chk_files.ps1"
+
 
 # determine update channel
 if ($env:APPVEYOR_PROJECT_NAME -match "(Nightly)") {
     write-host "UpdateChannel = Nightly"
     $UpdateChannel = "Nightly"
+    $ModifiedTagName = "$PreTagName-$TagName-NB"
 } elseif ($env:APPVEYOR_PROJECT_NAME -match "(Preview)") {
-    write-host "UpdateChannel = Nightly"
+    write-host "UpdateChannel = Preview"
     $UpdateChannel = "Preview"
+    $ModifiedTagName = "v$TagName-PB"
 } elseif ($env:APPVEYOR_PROJECT_NAME -match "(Stable)") {
-    write-host "UpdateChannel = Nightly"
+    write-host "UpdateChannel = Stable"
     $UpdateChannel = "Stable"
+    $ModifiedTagName = "v" + $TagName.Split("-")[0]
 } else {
     $UpdateChannel = ""
 }
@@ -107,25 +124,38 @@ if ($UpdateChannel -ne "" -and $buildFolder -ne "") {
     $releaseFolder = Join-Path -Path $PSScriptRoot -ChildPath "..\Release" -Resolve
     $msiFile = Get-ChildItem -Path "$buildFolder\*.msi" | Sort-Object LastWriteTime | Select-Object -last 1
     if (![string]::IsNullOrEmpty($msiFile)) {
-        $msiUpdateContents = New-MsiUpdateFileContent -MsiFile $msiFile -TagName $TagName
+        $msiUpdateContents = New-MsiUpdateFileContent -MsiFile $msiFile -TagName $ModifiedTagName
         $msiUpdateFileName = Resolve-UpdateCheckFileName -UpdateChannel $UpdateChannel -Type Normal
         Write-Output "`n`nMSI Update Check File Contents ($msiUpdateFileName)`n------------------------------"
         Tee-Object -InputObject $msiUpdateContents -FilePath "$releaseFolder\$msiUpdateFileName"
         write-host "msiUpdateFileName $releaseFolder\$msiUpdateFileName"
+        # commit msi update txt file
+        if ((Test-Path -Path "$releaseFolder\$msiUpdateFileName") -and (-not [string]::IsNullOrEmpty($WebsiteTargetRepository))) {
+            Write-Output "Publish $msiUpdateFileName to $WebsiteTargetRepository"
+            $update_file_content_string = Get-Content "$releaseFolder\$msiUpdateFileName" | Out-String
+            Set-GitHubContent -OwnerName $WebsiteTargetOwner -RepositoryName $WebsiteTargetRepository -Path $msiUpdateFileName -CommitMessage "Updating $msiUpdateFileName" -Content $update_file_content_string -BranchName main
+        }
     }
 
     # build zip update file
     $releaseFolder = Join-Path -Path $PSScriptRoot -ChildPath "..\Release" -Resolve
     $zipFile = Get-ChildItem -Path "$releaseFolder\*.zip" -Exclude "*-symbols-*.zip" | Sort-Object LastWriteTime | Select-Object -last 1
     if (![string]::IsNullOrEmpty($zipFile)) {
-        $zipUpdateContents = New-ZipUpdateFileContent -ZipFile $zipFile -TagName $TagName
+        $zipUpdateContents = New-ZipUpdateFileContent -ZipFile $zipFile -TagName $ModifiedTagName
         $zipUpdateFileName = Resolve-UpdateCheckFileName -UpdateChannel $UpdateChannel -Type Portable
         Write-Output "`n`nZip Update Check File Contents ($zipUpdateFileName)`n------------------------------"
         Tee-Object -InputObject $zipUpdateContents -FilePath "$releaseFolder\$zipUpdateFileName"
         write-host "zipUpdateFileName $releaseFolder\$zipUpdateFileName"
+        # commit zip update txt file
+        if ((Test-Path -Path "$releaseFolder\$zipUpdateFileName") -and (-not [string]::IsNullOrEmpty($WebsiteTargetRepository))) {
+            Write-Output "Publish $zipUpdateFileName to $WebsiteTargetRepository"
+            $update_file_content_string = Get-Content "$releaseFolder\$zipUpdateFileName" | Out-String
+            Set-GitHubContent -OwnerName $WebsiteTargetOwner -RepositoryName $WebsiteTargetRepository -Path $zipUpdateFileName -CommitMessage "Updating $zipUpdateFileName" -Content $update_file_content_string -BranchName main
+        }
     }
 } else {
     write-host "BuildFolder not found"
 }
+
 
 Write-Output "End create_upg_chk_files.ps1"
--- Tools/postbuild_mremoteng.ps1
+++ Tools/postbuild_mremoteng.ps1
@@ -25,6 +25,8 @@ param (
     $ExcludeFromSigning
 )
 
+. "$PSScriptRoot\github_functions.ps1"
+
 Write-Output "+===========================================================================================+"
 Write-Output "|                               Beginning mRemoteNG Post Build                              |"
 Write-Output "+===========================================================================================+"
@@ -54,10 +56,19 @@ Format-Table -AutoSize -Wrap -InputObject @{
 ###
 
 & "$PSScriptRoot\set_LargeAddressAware.ps1" -TargetDir $TargetDir -TargetFileName $TargetFileName
-& "$PSScriptRoot\verify_LargeAddressAware.ps1" -TargetDir $TargetDir -TargetFileName $TargetFileName
-& "$PSScriptRoot\tidy_files_for_release.ps1" -TargetDir $TargetDir -ConfigurationName $ConfigurationName
-& "$PSScriptRoot\sign_binaries.ps1" -TargetDir $TargetDir -CertificatePath $CertificatePath -CertificatePassword $CertificatePassword -ConfigurationName $ConfigurationName -Exclude $ExcludeFromSigning -SolutionDir $SolutionDir
-& "$PSScriptRoot\verify_binary_signatures.ps1" -TargetDir $TargetDir -ConfigurationName $ConfigurationName -CertificatePath $CertificatePath -SolutionDir $SolutionDir
-& "$PSScriptRoot\zip_files.ps1" -SolutionDir $SolutionDir -TargetDir $TargetDir -ConfigurationName $ConfigurationName
-& "$PSScriptRoot\create_upg_chk_files.ps1" -TagName $env:APPVEYOR_BUILD_VERSION -ProjectName $env:APPVEYOR_PROJECT_NAME
 
+& "$PSScriptRoot\verify_LargeAddressAware.ps1" -TargetDir $TargetDir -TargetFileName $TargetFileName
+
+& "$PSScriptRoot\tidy_files_for_release.ps1" -TargetDir $TargetDir -ConfigurationName $ConfigurationName
+
+& "$PSScriptRoot\sign_binaries.ps1" -TargetDir $TargetDir -CertificatePath $CertificatePath -CertificatePassword $CertificatePassword -ConfigurationName $ConfigurationName -Exclude $ExcludeFromSigning -SolutionDir $SolutionDir
+
+& "$PSScriptRoot\verify_binary_signatures.ps1" -TargetDir $TargetDir -ConfigurationName $ConfigurationName -CertificatePath $CertificatePath -SolutionDir $SolutionDir
+
+& "$PSScriptRoot\zip_files.ps1" -SolutionDir $SolutionDir -TargetDir $TargetDir -ConfigurationName $ConfigurationName
+
+& "$PSScriptRoot\create_upg_chk_files.ps1" -WebsiteTargetOwner $CURRENT_GITHUB_USER -WebsiteTargetRepository $env:WEBSITE_TARGET_REPOSITORY -PreTagName $env:NightlyBuildTagName -TagName $env:APPVEYOR_BUILD_VERSION -ProjectName $env:APPVEYOR_PROJECT_NAME
+
+#& "$PSScriptRoot\update_and_upload_website_release_json_file.ps1" -WebsiteTargetOwner $CURRENT_GITHUB_USER -WebsiteTargetRepository $env:WEBSITE_TARGET_REPOSITORY -PreTagName $env:NightlyBuildTagName -TagName $env:APPVEYOR_BUILD_VERSION -ProjectName $env:APPVEYOR_PROJECT_NAME
+
+Write-Output "End mRemoteNG Post Build"
