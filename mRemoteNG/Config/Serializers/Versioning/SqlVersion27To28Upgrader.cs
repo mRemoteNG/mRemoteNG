a926bccb97b3687d29df92b056d020925d7cab84
BlueBlock <theblueblock@protonmail.com>
2023-03-01 07:11:30 -0500

    fix db upgrade
    fix db upgrade 2.7->2.8

--- mRemoteNG/Config/Serializers/Versioning/SqlVersion27To28Upgrader.cs
+++ mRemoteNG/Config/Serializers/Versioning/SqlVersion27To28Upgrader.cs
@@ -25,21 +25,21 @@ namespace mRemoteNG.Config.Serializers.Versioning
             Runtime.MessageCollector.AddMessage(MessageClass.InformationMsg,
                                                 "Upgrading database from version 2.7 to version 2.8.");
 
-            const string mySqlText = @"
+            const string mySqlText1 = @"
 ALTER TABLE tblCons MODIFY COLUMN ID INT;
 ALTER TABLE tblCons DROP PRIMARY KEY, ADD PRIMARY KEY (ConstantID);
 ALTER TABLE tblCons ADD INDEX `id` (ID), MODIFY ID int auto_increment;
 ALTER TABLE tblCons MODIFY COLUMN ID INT AUTO_INCREMENT, ADD UNIQUE(ID);
 UPDATE tblRoot SET ConfVersion='2.8'";
+
             const string msSqlText1 = @"
-ALTER TABLE tblCons ADD  DEFAULT 0 FOR UseEnhancedMode;
-ALTER TABLE tblCons ADD  DEFAULT 0 FOR InheritUseEnhancedMode;
 UPDATE tblCons SET UseEnhancedMode = 0 WHERE UseEnhancedMode IS NULL;
 UPDATE tblCons SET InheritUseEnhancedMode = 0 WHERE InheritUseEnhancedMode IS NULL;
 ALTER TABLE tblCons ALTER COLUMN ConstantID varchar(128) NOT NULL;";
             const string msSqlText2 = @"
 ALTER TABLE tblCons ADD CONSTRAINT PK_tblCons PRIMARY KEY (ConstantID);
 UPDATE tblRoot SET ConfVersion='2.8';";
+
             DbCommand dbCommand;
 
             if (_databaseConnector.GetType() == typeof(MSSqlDatabaseConnector))
@@ -49,14 +49,12 @@ UPDATE tblRoot SET ConfVersion='2.8';";
                 dbCommand = _databaseConnector.DbCommand(msSqlText2);
             } else if (_databaseConnector.GetType() == typeof(MySqlDatabaseConnector))
             {
-                dbCommand = _databaseConnector.DbCommand(mySqlText);
+                dbCommand = _databaseConnector.DbCommand(mySqlText1);
             } else
             {
-                throw new Exception("Unknow database backend");
+                throw new Exception("Unknown database backend");
             }
-
-
-
+            
             dbCommand.ExecuteNonQuery();
             return new Version(2, 8);
         }
