95469107fd205d79b0a828aaab8c28308a20a2dc
kmscode <sean.kaim@gmail.com>
2023-03-05 15:37:49 -0500

    don't need to manually editbin for large address aware on x64 targets

--- Tools/postbuild_mremoteng.ps1
+++ Tools/postbuild_mremoteng.ps1
@@ -53,7 +53,8 @@ Format-Table -AutoSize -Wrap -InputObject @{
 # }
 ###
 
-& "$PSScriptRoot\set_LargeAddressAware.ps1" -TargetDir $TargetDir -TargetFileName $TargetFileName
+# Currently targeting x64, shouldn't need to manually set LargeAddressAware...
+#& "$PSScriptRoot\set_LargeAddressAware.ps1" -TargetDir $TargetDir -TargetFileName $TargetFileName
 & "$PSScriptRoot\verify_LargeAddressAware.ps1" -TargetDir $TargetDir -TargetFileName $TargetFileName
 & "$PSScriptRoot\tidy_files_for_release.ps1" -TargetDir $TargetDir -ConfigurationName $ConfigurationName
 & "$PSScriptRoot\sign_binaries.ps1" -TargetDir $TargetDir -CertificatePath $CertificatePath -CertificatePassword $CertificatePassword -ConfigurationName $ConfigurationName -Exclude $ExcludeFromSigning -SolutionDir $SolutionDir
