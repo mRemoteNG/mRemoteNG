16d182fca23fec17bb436df7a63f87702eb30ced
BlueBlock <theblueblock@protonmail.com>
2023-03-24 11:53:05 -0400

    Update NotificationPanelMessageWriter.cs
    fix notification bug, text not displaying

--- mRemoteNG/Messages/MessageWriters/NotificationPanelMessageWriter.cs
+++ mRemoteNG/Messages/MessageWriters/NotificationPanelMessageWriter.cs
@@ -13,28 +13,30 @@ namespace mRemoteNG.Messages.MessageWriters
 
         public NotificationPanelMessageWriter(ErrorAndInfoWindow messageWindow)
         {
-            if (messageWindow == null)
-                throw new ArgumentNullException(nameof(messageWindow));
-
-            _messageWindow = messageWindow;
+            _messageWindow = messageWindow ?? throw new ArgumentNullException(nameof(messageWindow));
         }
 
         public void Write(IMessage message)
         {
             var lvItem = new NotificationMessageListViewItem(message);
+
             AddToList(lvItem);
         }
 
         private void AddToList(ListViewItem lvItem)
         {
-            if (!_messageWindow.IsAccessible || _messageWindow.IsDisposed) { return; }
             if (_messageWindow.lvErrorCollector.InvokeRequired)
+            {
                 _messageWindow.lvErrorCollector.Invoke((MethodInvoker)(() => AddToList(lvItem)));
+            }
             else
             {
                 _messageWindow.lvErrorCollector.Items.Insert(0, lvItem);
+
                 if (_messageWindow.lvErrorCollector.Items.Count > 0)
+                {
                     _messageWindow.pbError.Visible = true;
+                }
             }
         }
     }
